57d9a43105873f4c4cafee77fe98913c463e009f
GitHub <noreply@github.com>
2020-09-17 15:06:22 +0000

    95e168603 test: basic e2e for b2b checkout (#8912)

--- src/cms/page/content-page-meta.resolver.d.ts
+++ src/cms/page/content-page-meta.resolver.d.ts
@@ -11,7 +11,6 @@ import { PageBreadcrumbResolver, PageTitleResolver } from './page.resolvers';
  *
  * The page title, and breadcrumbs are resolved in this implementation only.
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class ContentPageMetaResolver extends PageMetaResolver implements PageTitleResolver, PageBreadcrumbResolver {
     protected cms: CmsService;
     protected translation: TranslationService;
@@ -28,7 +27,4 @@ export declare class ContentPageMetaResolver extends PageMetaResolver implements
      * The home page label is resolved from the translation service.
      */
     resolveBreadcrumbs(): Observable<BreadcrumbMeta[]>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ContentPageMetaResolver, never>;
 }
-
-//# sourceMappingURL=content-page-meta.resolver.d.ts.map
--- src/product/facade/searchbox.service.d.ts
+++ src/product/facade/searchbox.service.d.ts
@@ -2,7 +2,6 @@ import { Observable } from 'rxjs';
 import { ProductSearchPage, Suggestion } from '../../model/index';
 import { SearchConfig } from '../model/index';
 import { ProductSearchService } from './product-search.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class SearchboxService extends ProductSearchService {
     /**
      * dispatch the search for the search box
@@ -15,7 +14,4 @@ export declare class SearchboxService extends ProductSearchService {
     clearResults(): void;
     getSuggestionResults(): Observable<Suggestion[]>;
     searchSuggestions(query: string, searchConfig?: SearchConfig): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<SearchboxService, never>;
 }
-
-//# sourceMappingURL=searchbox.service.d.ts.map
