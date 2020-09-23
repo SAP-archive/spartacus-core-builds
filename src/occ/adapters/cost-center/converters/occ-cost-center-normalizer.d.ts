5d1fcc1556f72111a06cbc03bf47a79b80152ed9
GitHub <noreply@github.com>
2020-09-23 19:02:07 +0000

    9ebc055de fix: infinite loop when loading anonymous consents (#8989)

--- src/occ/adapters/cost-center/converters/occ-cost-center-normalizer.d.ts
+++ src/occ/adapters/cost-center/converters/occ-cost-center-normalizer.d.ts
@@ -1,11 +1,6 @@
 import { Occ } from '../../../occ-models/occ.models';
 import { Converter } from '../../../../util/converter.service';
 import { CostCenter } from '../../../../model/org-unit.model';
-import * as ɵngcc0 from '@angular/core';
 export declare class OccCostCenterNormalizer implements Converter<Occ.CostCenter, CostCenter> {
     convert(source: Occ.CostCenter, target?: CostCenter): CostCenter;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCostCenterNormalizer, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<OccCostCenterNormalizer>;
 }
-
-//# sourceMappingURL=occ-cost-center-normalizer.d.ts.map
