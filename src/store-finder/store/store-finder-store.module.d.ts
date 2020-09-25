5fa2edb2a4a7e0dbac0eba4dcd52317bb4dfe048
GitHub <noreply@github.com>
2020-09-25 13:49:10 +0000

    3dec74026 feat: Allow data passing to  components using the launch dialog service (GH-9041)

--- src/store-finder/store/store-finder-store.module.d.ts
+++ src/store-finder/store/store-finder-store.module.d.ts
@@ -1,11 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@angular/common/http';
-import * as ɵngcc3 from '@ngrx/store';
-import * as ɵngcc4 from '@ngrx/effects';
 export declare class StoreFinderStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StoreFinderStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule, typeof ɵngcc3.StoreFeatureModule, typeof ɵngcc4.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<StoreFinderStoreModule>;
 }
-
-//# sourceMappingURL=store-finder-store.module.d.ts.map
--- src/user/facade/user-consent.service.d.ts
+++ src/user/facade/user-consent.service.d.ts
@@ -4,7 +4,6 @@ import { AuthService } from '../../auth/facade/auth.service';
 import { Consent, ConsentTemplate } from '../../model/consent.model';
 import { StateWithProcess } from '../../process/store/process-state';
 import { StateWithUser } from '../store/user-state';
-import * as ɵngcc0 from '@angular/core';
 export declare class UserConsentService {
     protected store: Store<StateWithUser | StateWithProcess<void>>;
     protected authService: AuthService;
@@ -107,7 +106,4 @@ export declare class UserConsentService {
      * @param hideTemplateIds template IDs to hide
      */
     filterConsentTemplates(templateList: ConsentTemplate[], hideTemplateIds?: string[]): ConsentTemplate[];
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserConsentService, never>;
 }
-
-//# sourceMappingURL=user-consent.service.d.ts.map
