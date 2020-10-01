7a1e50eb8a896b9ef59c870a08f2c75fd71d32a9
GitHub <noreply@github.com>
2020-10-01 14:54:51 +0000

    9cffe9999 feat: Active state update on facade #8976 (#9048)

--- src/user/store/effects/order-details.effect.d.ts
+++ src/user/store/effects/order-details.effect.d.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { UserOrderConnector } from '../../connectors/order/user-order.connector';
 import { UserActions } from '../actions/index';
 import { GlobalMessageService } from '../../../global-message/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class OrderDetailsEffect {
     private actions$;
     private orderConnector;
@@ -11,8 +10,4 @@ export declare class OrderDetailsEffect {
     loadOrderDetails$: Observable<UserActions.OrderDetailsAction>;
     cancelOrder$: Observable<UserActions.OrderDetailsAction>;
     constructor(actions$: Actions, orderConnector: UserOrderConnector, globalMessageService: GlobalMessageService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OrderDetailsEffect, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<OrderDetailsEffect>;
 }
-
-//# sourceMappingURL=order-details.effect.d.ts.map
