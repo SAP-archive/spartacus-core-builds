64c87115faa366c10e92d5cda717576a0d712a3b
GitHub <noreply@github.com>
2020-10-01 14:49:23 +0000

    9cffe9999 feat: Active state update on facade #8976 (#9048)

--- src/auth/guards/auth.guard.d.ts
+++ src/auth/guards/auth.guard.d.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { RoutingService } from '../../routing/facade/routing.service';
 import { AuthService } from '../facade/auth.service';
 import { AuthRedirectService } from './auth-redirect.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class AuthGuard implements CanActivate {
     protected routingService: RoutingService;
     protected authService: AuthService;
@@ -10,4 +11,7 @@ export declare class AuthGuard implements CanActivate {
     protected router: Router;
     constructor(routingService: RoutingService, authService: AuthService, authRedirectService: AuthRedirectService, router: Router);
     canActivate(): Observable<boolean>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<AuthGuard, never>;
 }
+
+//# sourceMappingURL=auth.guard.d.ts.map
--- src/auth/services/user-authentication/user-authentication-token.service.d.ts
+++ src/auth/services/user-authentication/user-authentication-token.service.d.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { OccEndpointsService } from '../../../occ/services/occ-endpoints.service';
 import { AuthConfig } from '../../config/auth-config';
 import { UserToken } from '../../models/token-types.model';
+import * as ɵngcc0 from '@angular/core';
 export declare class UserAuthenticationTokenService {
     protected http: HttpClient;
     protected config: AuthConfig;
@@ -11,4 +12,7 @@ export declare class UserAuthenticationTokenService {
     loadToken(userId: string, password: string): Observable<UserToken>;
     refreshToken(refreshToken: string): Observable<UserToken>;
     revoke(userToken: UserToken): Observable<{}>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserAuthenticationTokenService, never>;
 }
+
+//# sourceMappingURL=user-authentication-token.service.d.ts.map
