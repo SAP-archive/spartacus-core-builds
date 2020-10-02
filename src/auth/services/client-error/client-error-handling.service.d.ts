026dc92a7b06faa5eabd14eec7fc5962d16a462c
GitHub <noreply@github.com>
2020-10-02 19:22:33 +0000

    4b697c972 chore: Update dep with security warning in api extractor (#9130)

--- src/auth/services/client-error/client-error-handling.service.d.ts
+++ src/auth/services/client-error/client-error-handling.service.d.ts
@@ -2,13 +2,9 @@ import { HttpRequest, HttpHandler } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../facade/auth.service';
 import { ClientToken } from '../../models/token-types.model';
-import * as ɵngcc0 from '@angular/core';
 export declare class ClientErrorHandlingService {
     protected authService: AuthService;
     constructor(authService: AuthService);
     handleExpiredClientToken(request: HttpRequest<any>, next: HttpHandler): Observable<any>;
     protected createNewRequestWithNewToken(request: HttpRequest<any>, token: ClientToken): HttpRequest<any>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ClientErrorHandlingService, never>;
 }
-
-//# sourceMappingURL=client-error-handling.service.d.ts.map
--- src/kyma/store/kyma-store.module.d.ts
+++ src/kyma/store/kyma-store.module.d.ts
@@ -1,14 +1,4 @@
 import { StateConfig } from '../../state/config/state-config';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@angular/common/http';
-import * as ɵngcc3 from '../../state/state.module';
-import * as ɵngcc4 from '@ngrx/store';
-import * as ɵngcc5 from '@ngrx/effects';
 export declare function kymaStoreConfigFactory(): StateConfig;
 export declare class KymaStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<KymaStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule, typeof ɵngcc3.StateModule, typeof ɵngcc4.StoreFeatureModule, typeof ɵngcc5.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<KymaStoreModule>;
 }
-
-//# sourceMappingURL=kyma-store.module.d.ts.map
