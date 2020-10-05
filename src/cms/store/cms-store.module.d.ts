03a4558a0d5e408685bed0b401b8843647d5857f
GitHub <noreply@github.com>
2020-10-05 10:49:54 +0000

    7a590dcee Avoid NPE on destructuring (#9165)

--- src/cms/store/cms-store.module.d.ts
+++ src/cms/store/cms-store.module.d.ts
@@ -1,14 +1,4 @@
 import { StateConfig } from '../../state/config/state-config';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@angular/common/http';
-import * as ɵngcc3 from '../../state/state.module';
-import * as ɵngcc4 from '@ngrx/store';
-import * as ɵngcc5 from '@ngrx/effects';
 export declare function cmsStoreConfigFactory(): StateConfig;
 export declare class CmsStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CmsStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule, typeof ɵngcc3.StateModule, typeof ɵngcc4.StoreFeatureModule, typeof ɵngcc5.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CmsStoreModule>;
 }
-
-//# sourceMappingURL=cms-store.module.d.ts.map
