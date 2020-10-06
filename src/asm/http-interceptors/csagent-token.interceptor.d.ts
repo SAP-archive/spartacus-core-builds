e963e01954f983d3d9b08a99c41d01ad19f3e22a
GitHub <noreply@github.com>
2020-10-06 12:41:32 +0000

    5665b5e21 fix: locales must be driven by the domain entity rather than the table configuration type (#9209)

--- src/asm/http-interceptors/csagent-token.interceptor.d.ts
+++ src/asm/http-interceptors/csagent-token.interceptor.d.ts
@@ -1,13 +1,9 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AsmAuthService } from '../facade/asm-auth.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class CustomerSupportAgentTokenInterceptor implements HttpInterceptor {
     private asmAuthService;
     constructor(asmAuthService: AsmAuthService);
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
     private getCustomerSupportAgentToken;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CustomerSupportAgentTokenInterceptor, never>;
 }
-
-//# sourceMappingURL=csagent-token.interceptor.d.ts.map
--- src/kyma/config/kyma-config.d.ts.__ivy_ngcc_bak
+++
@@ -1,7 +0,0 @@
-import { OccConfig } from '../../occ/config/occ-config';
-export declare abstract class KymaConfig extends OccConfig {
-    authentication?: {
-        kyma_client_id?: string;
-        kyma_client_secret?: string;
-    };
-}
