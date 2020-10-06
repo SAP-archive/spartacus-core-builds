901cbb10957d8d4f7c4b8fc4f12a0d2536429721
GitHub <noreply@github.com>
2020-10-06 14:15:06 +0000

    b807da746 GH-8750 No need to clone repo when installing libs from npm (#8751)

--- src/user/store/effects/replenishment-order-details.effect.d.ts
+++ src/user/store/effects/replenishment-order-details.effect.d.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { GlobalMessageService } from '../../../global-message/index';
 import { UserReplenishmentOrderConnector } from '../../connectors/replenishment-order/user-replenishment-order.connector';
 import { UserActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class ReplenishmentOrderDetailsEffect {
     private actions$;
     private replenishmentOrderConnector;
@@ -11,8 +10,4 @@ export declare class ReplenishmentOrderDetailsEffect {
     loadReplenishmentOrderDetails$: Observable<UserActions.ReplenishmentOrderDetailsAction>;
     cancelReplenishmentOrder$: Observable<UserActions.ReplenishmentOrderDetailsAction>;
     constructor(actions$: Actions, replenishmentOrderConnector: UserReplenishmentOrderConnector, globalMessageService: GlobalMessageService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ReplenishmentOrderDetailsEffect, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<ReplenishmentOrderDetailsEffect>;
 }
-
-//# sourceMappingURL=replenishment-order-details.effect.d.ts.map
