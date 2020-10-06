2dee1d1e151ab010191b5d5eac6a55a32a6bda3b
GitHub <noreply@github.com>
2020-10-06 12:33:46 +0000

    5665b5e21 fix: locales must be driven by the domain entity rather than the table configuration type (#9209)

--- src/store-finder/config/store-finder-config.d.ts
+++ src/store-finder/config/store-finder-config.d.ts
@@ -1,3 +1,4 @@
+import * as ɵngcc0 from '@angular/core';
 export declare abstract class StoreFinderConfig {
     googleMaps?: {
         apiUrl?: string;
@@ -6,4 +7,7 @@ export declare abstract class StoreFinderConfig {
         selectedMarkerScale?: number;
         radius?: number;
     };
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<StoreFinderConfig, never>;
 }
+
+//# sourceMappingURL=store-finder-config.d.ts.map
