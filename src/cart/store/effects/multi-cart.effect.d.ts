a2d78a18f1deda38ea0cb29f6a04271364257eda
GitHub <noreply@github.com>
2020-10-06 11:46:30 +0000

    09a0f7cee cleanup tests (#9208)

--- src/cart/store/effects/multi-cart.effect.d.ts
+++ src/cart/store/effects/multi-cart.effect.d.ts
@@ -1,14 +1,9 @@
 import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { CartActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class MultiCartEffects {
     private actions$;
     setTempCart$: Observable<CartActions.RemoveCart>;
     processesIncrement$: Observable<CartActions.CartProcessesIncrement>;
     constructor(actions$: Actions);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<MultiCartEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<MultiCartEffects>;
 }
-
-//# sourceMappingURL=multi-cart.effect.d.ts.map
--- src/i18n/testing/mock-translate.pipe.d.ts
+++ src/i18n/testing/mock-translate.pipe.d.ts
@@ -1,10 +1,5 @@
 import { PipeTransform } from '@angular/core';
 import { Translatable } from '../translatable';
-import * as ɵngcc0 from '@angular/core';
 export declare class MockTranslatePipe implements PipeTransform {
     transform(input: Translatable | string, options?: object): string;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<MockTranslatePipe, never>;
-    static ɵpipe: ɵngcc0.ɵɵPipeDefWithMeta<MockTranslatePipe, "cxTranslate">;
 }
-
-//# sourceMappingURL=mock-translate.pipe.d.ts.map
