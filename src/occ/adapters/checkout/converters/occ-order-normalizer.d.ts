ed3c7a6f07a0efca24c7d4f6aab8136a38155e3a
GitHub <noreply@github.com>
2020-10-06 14:08:29 +0000

    b807da746 GH-8750 No need to clone repo when installing libs from npm (#8751)

--- src/occ/adapters/checkout/converters/occ-order-normalizer.d.ts
+++ src/occ/adapters/checkout/converters/occ-order-normalizer.d.ts
@@ -1,9 +1,13 @@
 import { Order } from '../../../../model/order.model';
 import { Converter, ConverterService } from '../../../../util/converter.service';
 import { Occ } from '../../../occ-models/occ.models';
+import * as ɵngcc0 from '@angular/core';
 export declare class OccOrderNormalizer implements Converter<Occ.Order, Order> {
     private converter;
     constructor(converter: ConverterService);
     convert(source: Occ.Order, target?: Order): Order;
     private convertOrderEntry;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccOrderNormalizer, never>;
 }
+
+//# sourceMappingURL=occ-order-normalizer.d.ts.map
