e15b3af5a6b8b870cea677671ad7b65e98868dc7
GitHub <noreply@github.com>
2020-10-07 05:40:32 +0000

    e49e3b237 fix: directionality in breadcrumbs (#9218)

--- src/auth/config/auth-config.d.ts
+++ src/auth/config/auth-config.d.ts
@@ -1,11 +1,7 @@
 import { OccConfig } from '../../occ/config/occ-config';
-import * as ɵngcc0 from '@angular/core';
 export declare abstract class AuthConfig extends OccConfig {
     authentication?: {
         client_id?: string;
         client_secret?: string;
     };
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AuthConfig, never>;
 }
-
-//# sourceMappingURL=auth-config.d.ts.map
