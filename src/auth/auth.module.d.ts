f3cfab2c9bd93e3f60297783fdb1b04a7326fdf1
GitHub <noreply@github.com>
2020-10-07 13:14:18 +0000

    d3f05465f Major release checklist (#9096)

--- src/auth/auth.module.d.ts
+++ src/auth/auth.module.d.ts
@@ -1,4 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from '@angular/common';
+import * as ɵngcc2 from '@angular/common/http';
+import * as ɵngcc3 from './store/auth-store.module';
 export declare class AuthModule {
     static forRoot(): ModuleWithProviders<AuthModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AuthModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule, typeof ɵngcc3.AuthStoreModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<AuthModule>;
 }
+
+//# sourceMappingURL=auth.module.d.ts.map
