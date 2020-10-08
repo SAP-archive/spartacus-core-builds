49063f881d97f89545d52b3dcfdec09d4d7b10e0
GitHub <noreply@github.com>
2020-10-08 06:53:53 +0000

    532603fbc Release/3.0.0 next.3 (#9235)

--- src/checkout/store/checkout-store.module.d.ts
+++ src/checkout/store/checkout-store.module.d.ts
@@ -1,11 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@angular/common/http';
-import * as ɵngcc3 from '@ngrx/store';
-import * as ɵngcc4 from '@ngrx/effects';
 export declare class CheckoutStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CheckoutStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule, typeof ɵngcc3.StoreFeatureModule, typeof ɵngcc4.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CheckoutStoreModule>;
 }
-
-//# sourceMappingURL=checkout-store.module.d.ts.map
--- src/user/store/effects/product-interests.effect.d.ts
+++ src/user/store/effects/product-interests.effect.d.ts
@@ -3,7 +3,6 @@ import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { UserActions } from '../actions/index';
 import { UserInterestsConnector } from '../../connectors/interests/user-interests.connector';
-import * as ɵngcc0 from '@angular/core';
 export declare class ProductInterestsEffect {
     private actions$;
     private userInterestsConnector;
@@ -11,8 +10,4 @@ export declare class ProductInterestsEffect {
     loadProductInteres$: Observable<UserActions.ProductInterestsAction>;
     removeProductInterest$: Observable<Action>;
     addProductInterest$: Observable<Action>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductInterestsEffect, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<ProductInterestsEffect>;
 }
-
-//# sourceMappingURL=product-interests.effect.d.ts.map
