5509ce99965f5090191f6562c01667fc18a791de
GitHub <noreply@github.com>
2020-10-09 17:18:18 +0000

    d156713e1 test: A11y tests for b2b (#9124)

--- src/occ/adapters/user/user-occ.module.d.ts
+++ src/occ/adapters/user/user-occ.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@angular/common/http';
 export declare class UserOccModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<UserOccModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<UserOccModule>;
 }
-
-//# sourceMappingURL=user-occ.module.d.ts.map
