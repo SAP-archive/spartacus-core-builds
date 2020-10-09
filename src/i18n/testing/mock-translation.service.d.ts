087a6d5cbc83fbcc0ed993d59a549ca9246bb542
GitHub <noreply@github.com>
2020-10-09 06:05:50 +0000

    2846ef579 feat: Create child unit (#9229)

--- src/i18n/testing/mock-translation.service.d.ts
+++ src/i18n/testing/mock-translation.service.d.ts
@@ -1,6 +1,11 @@
 import { Observable } from 'rxjs';
 import { TranslationService } from '../translation.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class MockTranslationService implements TranslationService {
     translate(key: string, options?: any, _whitespaceUntilLoaded?: boolean): Observable<string>;
     loadChunks(_chunks: string | string[]): Promise<any>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<MockTranslationService, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<MockTranslationService>;
 }
+
+//# sourceMappingURL=mock-translation.service.d.ts.map
