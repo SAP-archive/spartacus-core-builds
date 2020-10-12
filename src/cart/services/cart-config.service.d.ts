7cafeba76f319c91a5f56fe3817de7a8d00699cc
GitHub <noreply@github.com>
2020-10-12 13:11:52 +0000

    b4688d377 chore: Adjust scripting to new sampledata naming convention (#9292)

--- src/cart/services/cart-config.service.d.ts
+++ src/cart/services/cart-config.service.d.ts
@@ -1,10 +1,6 @@
 import { CartConfig } from '../config/cart-config';
-import * as ɵngcc0 from '@angular/core';
 export declare class CartConfigService {
     protected config: CartConfig;
     constructor(config: CartConfig);
     isSelectiveCartEnabled(): boolean;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CartConfigService, never>;
 }
-
-//# sourceMappingURL=cart-config.service.d.ts.map
