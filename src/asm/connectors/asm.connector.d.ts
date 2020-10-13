921fedd96717facda884bbf248998dc1a0ff4aa8
GitHub <noreply@github.com>
2020-10-13 09:25:01 +0000

    45c2f8386 Updated ccv2 base url (#9296)

--- src/asm/connectors/asm.connector.d.ts
+++ src/asm/connectors/asm.connector.d.ts
@@ -1,8 +1,12 @@
 import { Observable } from 'rxjs';
 import { CustomerSearchOptions, CustomerSearchPage } from '../models/asm.models';
 import { AsmAdapter } from './asm.adapter';
+import * as ɵngcc0 from '@angular/core';
 export declare class AsmConnector {
     protected asmAdapter: AsmAdapter;
     constructor(asmAdapter: AsmAdapter);
     customerSearch(options: CustomerSearchOptions): Observable<CustomerSearchPage>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<AsmConnector, never>;
 }
+
+//# sourceMappingURL=asm.connector.d.ts.map
