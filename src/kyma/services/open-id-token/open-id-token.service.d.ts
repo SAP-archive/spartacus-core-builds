209c512cec8a88befefa54d1a410f8523769f46a
GitHub <noreply@github.com>
2020-10-13 22:05:25 +0000

    2dff81ad4 Update Slack invite link (#9311)

--- src/kyma/services/open-id-token/open-id-token.service.d.ts
+++ src/kyma/services/open-id-token/open-id-token.service.d.ts
@@ -2,10 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { KymaConfig } from '../../config/kyma-config';
 import { OpenIdToken } from '../../models/kyma-token-types.model';
+import * as ɵngcc0 from '@angular/core';
 export declare class OpenIdAuthenticationTokenService {
     private config;
     private http;
     constructor(config: KymaConfig, http: HttpClient);
     loadOpenIdAuthenticationToken(username: string, password: string): Observable<OpenIdToken>;
     private getOAuthEndpoint;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OpenIdAuthenticationTokenService, never>;
 }
+
+//# sourceMappingURL=open-id-token.service.d.ts.map
