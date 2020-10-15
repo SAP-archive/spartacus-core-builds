ba111a1edfb61f7d21600b0bb022b277058c384c
GitHub <noreply@github.com>
2020-10-15 18:42:32 +0000

    94e0bd2e1 fix: Update to bootstrap 4.3.1 or higher  (#9307)

--- src/asm/asm.module.d.ts
+++ src/asm/asm.module.d.ts
@@ -1,11 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from './store/asm-store.module';
 export declare class AsmModule {
     static forRoot(): ModuleWithProviders<AsmModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AsmModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.AsmStoreModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<AsmModule>;
 }
-
-//# sourceMappingURL=asm.module.d.ts.map
