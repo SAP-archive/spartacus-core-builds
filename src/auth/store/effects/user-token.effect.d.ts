4b816ee942ffee4475417ba98dec1fb6a2020a06
GitHub <noreply@github.com>
2020-10-15 09:37:49 +0000

    8bd1f9719 Move page template style class to directive (#9285)

--- src/auth/store/effects/user-token.effect.d.ts
+++ src/auth/store/effects/user-token.effect.d.ts
@@ -2,7 +2,6 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { UserAuthenticationTokenService } from '../../services/user-authentication/user-authentication-token.service';
 import { AuthActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class UserTokenEffects {
     private actions$;
     private userTokenService;
@@ -11,8 +10,4 @@ export declare class UserTokenEffects {
     refreshUserToken$: Observable<AuthActions.UserTokenAction>;
     revokeUserToken$: Observable<AuthActions.UserTokenAction>;
     constructor(actions$: Actions, userTokenService: UserAuthenticationTokenService);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserTokenEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<UserTokenEffects>;
 }
-
-//# sourceMappingURL=user-token.effect.d.ts.map
