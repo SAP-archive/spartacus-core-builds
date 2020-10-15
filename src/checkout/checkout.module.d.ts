36a856307a00ff2ee7fa363a6d915ada39e4bda6
GitHub <noreply@github.com>
2020-10-15 11:49:35 +0000

    2955be184 refactor: unit tree component (#8894)

--- src/checkout/checkout.module.d.ts
+++ src/checkout/checkout.module.d.ts
@@ -1,11 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './store/checkout-store.module';
-import * as ɵngcc2 from './events/checkout-event.module';
 export declare class CheckoutModule {
     static forRoot(): ModuleWithProviders<CheckoutModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CheckoutModule, never, [typeof ɵngcc1.CheckoutStoreModule, typeof ɵngcc2.CheckoutEventModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CheckoutModule>;
 }
-
-//# sourceMappingURL=checkout.module.d.ts.map
--- src/site-context/store/site-context-store.module.d.ts
+++ src/site-context/store/site-context-store.module.d.ts
@@ -1,13 +1,4 @@
 import { StateConfig } from '../../state/config/state-config';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@angular/common/http';
-import * as ɵngcc3 from '@ngrx/store';
-import * as ɵngcc4 from '@ngrx/effects';
 export declare function siteContextStoreConfigFactory(): StateConfig;
 export declare class SiteContextStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<SiteContextStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule, typeof ɵngcc3.StoreFeatureModule, typeof ɵngcc4.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<SiteContextStoreModule>;
 }
-
-//# sourceMappingURL=site-context-store.module.d.ts.map
