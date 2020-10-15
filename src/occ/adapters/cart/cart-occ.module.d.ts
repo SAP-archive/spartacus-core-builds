b390eb7b3a19f78723915088e3b84ada7fc021ee
GitHub <noreply@github.com>
2020-10-15 13:12:34 +0000

    8766e0787 fix: improve visible focus extensibility (#9342)

--- src/occ/adapters/cart/cart-occ.module.d.ts
+++ src/occ/adapters/cart/cart-occ.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@angular/common/http';
 export declare class CartOccModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CartOccModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.HttpClientModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CartOccModule>;
 }
-
-//# sourceMappingURL=cart-occ.module.d.ts.map
