162eb233fb71756f957e0affe19307f8687a8e89
GitHub <noreply@github.com>
2020-10-15 08:22:27 +0000

    d4fd6cee4 Corrected the public instance urls (#9328)

--- src/auth/http-interceptors/user-token.interceptor.d.ts
+++ src/auth/http-interceptors/user-token.interceptor.d.ts
@@ -2,14 +2,10 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs';
 import { OccEndpointsService } from '../../occ/services/occ-endpoints.service';
 import { AuthService } from '../facade/auth.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class UserTokenInterceptor implements HttpInterceptor {
     private authService;
     private occEndpoints;
     constructor(authService: AuthService, occEndpoints: OccEndpointsService);
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
     private isOccUrl;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserTokenInterceptor, never>;
 }
-
-//# sourceMappingURL=user-token.interceptor.d.ts.map
--- src/occ/adapters/product/converters/product-name-normalizer.d.ts
+++ src/occ/adapters/product/converters/product-name-normalizer.d.ts
@@ -2,13 +2,9 @@ import { OccConfig } from '../../../config/occ-config';
 import { Occ } from '../../../occ-models/occ.models';
 import { Converter } from '../../../../util/converter.service';
 import { Product } from '../../../../model/product.model';
-import * as ɵngcc0 from '@angular/core';
 export declare class ProductNameNormalizer implements Converter<Occ.Product, Product> {
     protected config: OccConfig;
     constructor(config: OccConfig);
     convert(source: Occ.Product, target?: Product): Product;
     protected normalize(name: string): string;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductNameNormalizer, never>;
 }
-
-//# sourceMappingURL=product-name-normalizer.d.ts.map
