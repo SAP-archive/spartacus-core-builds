ad38b513c3d483c84d64b07ed8917f8ca4b18c90
GitHub <noreply@github.com>
2020-10-16 10:09:15 +0000

    8db3a95cd fix: avoid empty state after changing own email on user list, clear store data explicit calls refactor (#8988)

--- src/routing/configurable-routes/url-translation/url.pipe.d.ts
+++ src/routing/configurable-routes/url-translation/url.pipe.d.ts
@@ -1,8 +1,13 @@
 import { PipeTransform } from '@angular/core';
 import { SemanticPathService } from './semantic-path.service';
 import { UrlCommands } from './url-command';
+import * as ɵngcc0 from '@angular/core';
 export declare class UrlPipe implements PipeTransform {
     private urlService;
     constructor(urlService: SemanticPathService);
     transform(commands: UrlCommands): any[];
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<UrlPipe, never>;
+    static ɵpipe: ɵngcc0.ɵɵPipeDefWithMeta<UrlPipe, "cxUrl">;
 }
+
+//# sourceMappingURL=url.pipe.d.ts.map
