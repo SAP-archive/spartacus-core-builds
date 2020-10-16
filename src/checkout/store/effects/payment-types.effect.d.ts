c47ff27e5fd5a01f44c755d007219c7a7deed122
GitHub <noreply@github.com>
2020-10-16 12:33:42 +0000

    8d3e5da18 feat: show spinner when continue button pressed in Delivery Mode step (GH-9294)

--- src/checkout/store/effects/payment-types.effect.d.ts
+++ src/checkout/store/effects/payment-types.effect.d.ts
@@ -3,10 +3,15 @@ import { Observable } from 'rxjs';
 import { CartActions } from '../../../cart/store/actions/index';
 import { PaymentTypeConnector } from '../../connectors/payment-type/payment-type.connector';
 import { CheckoutActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class PaymentTypesEffects {
     private actions$;
     private paymentTypeConnector;
     loadPaymentTypes$: Observable<CheckoutActions.LoadPaymentTypesSuccess | CheckoutActions.LoadPaymentTypesFail>;
     setPaymentType$: Observable<CheckoutActions.SetPaymentTypeSuccess | CheckoutActions.SetPaymentTypeFail | CartActions.LoadCartSuccess | CheckoutActions.ClearCheckoutData>;
     constructor(actions$: Actions, paymentTypeConnector: PaymentTypeConnector);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<PaymentTypesEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<PaymentTypesEffects>;
 }
+
+//# sourceMappingURL=payment-types.effect.d.ts.map
