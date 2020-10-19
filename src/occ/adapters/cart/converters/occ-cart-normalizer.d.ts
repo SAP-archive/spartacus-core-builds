5bd91492058f7b68981b66b4c97319e7d740650b
GitHub <noreply@github.com>
2020-10-19 19:34:34 +0000

    c992bad4e fix: Disable permission type select in Purchase limit edit form #9354 (#9405)

--- src/occ/adapters/cart/converters/occ-cart-normalizer.d.ts
+++ src/occ/adapters/cart/converters/occ-cart-normalizer.d.ts
@@ -1,6 +1,7 @@
 import { Cart } from '../../../../model/cart.model';
 import { Converter, ConverterService } from '../../../../util/converter.service';
 import { Occ } from '../../../occ-models/occ.models';
+import * as ɵngcc0 from '@angular/core';
 export declare class OccCartNormalizer implements Converter<Occ.Cart, Cart> {
     private converter;
     constructor(converter: ConverterService);
@@ -10,4 +11,7 @@ export declare class OccCartNormalizer implements Converter<Occ.Cart, Cart> {
      */
     private removeDuplicatePromotions;
     private removeDuplicateItems;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCartNormalizer, never>;
 }
+
+//# sourceMappingURL=occ-cart-normalizer.d.ts.map
