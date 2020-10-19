ee10822980d949f80f19a319b31d1148852c3d23
GitHub <noreply@github.com>
2020-10-19 18:19:30 +0000

    736066b3c fix: Prevent API requests to be called with anonymous user

--- src/checkout/connectors/payment-type/payment-type.connector.d.ts
+++ src/checkout/connectors/payment-type/payment-type.connector.d.ts
@@ -1,9 +1,13 @@
 import { Observable } from 'rxjs';
 import { PaymentTypeAdapter } from './payment-type.adapter';
 import { PaymentType } from '../../../model/cart.model';
+import * as ɵngcc0 from '@angular/core';
 export declare class PaymentTypeConnector {
     protected adapter: PaymentTypeAdapter;
     constructor(adapter: PaymentTypeAdapter);
     getPaymentTypes(): Observable<PaymentType[]>;
     setPaymentType(userId: string, cartId: string, typeCode: string, poNumber?: string): Observable<any>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<PaymentTypeConnector, never>;
 }
+
+//# sourceMappingURL=payment-type.connector.d.ts.map
--- src/user/facade/user-consent.service.d.ts
+++ src/user/facade/user-consent.service.d.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../auth/facade/auth.service';
 import { Consent, ConsentTemplate } from '../../model/consent.model';
 import { StateWithProcess } from '../../process/store/process-state';
 import { StateWithUser } from '../store/user-state';
+import * as ɵngcc0 from '@angular/core';
 export declare class UserConsentService {
     protected store: Store<StateWithUser | StateWithProcess<void>>;
     protected authService: AuthService;
@@ -106,4 +107,7 @@ export declare class UserConsentService {
      * @param hideTemplateIds template IDs to hide
      */
     filterConsentTemplates(templateList: ConsentTemplate[], hideTemplateIds?: string[]): ConsentTemplate[];
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserConsentService, never>;
 }
+
+//# sourceMappingURL=user-consent.service.d.ts.map
