eb61e75a4820b1b64635e09b852707a6ba4357c1
GitHub <noreply@github.com>
2020-10-20 15:18:25 +0000

    5e43e0a89 fix: adjusting max-width limit for search box (#9412)

--- src/kyma/store/effects/open-id-token.effect.d.ts
+++ src/kyma/store/effects/open-id-token.effect.d.ts
@@ -2,10 +2,15 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { OpenIdAuthenticationTokenService } from '../../services/open-id-token/open-id-token.service';
 import { KymaActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class OpenIdTokenEffect {
     private actions$;
     private openIdTokenService;
     triggerOpenIdTokenLoading$: Observable<KymaActions.LoadOpenIdToken>;
     loadOpenIdToken$: Observable<KymaActions.OpenIdTokenActions>;
     constructor(actions$: Actions, openIdTokenService: OpenIdAuthenticationTokenService);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OpenIdTokenEffect, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<OpenIdTokenEffect>;
 }
+
+//# sourceMappingURL=open-id-token.effect.d.ts.map
