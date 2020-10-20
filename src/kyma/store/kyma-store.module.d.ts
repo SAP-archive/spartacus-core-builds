31eb4130eddc84c5626f75f464f1e8fa133c5837
GitHub <noreply@github.com>
2020-10-20 12:23:35 +0000

    8c8c4e562 feat: My Company E2E Skilet and Some Tests (#7845)

--- src/kyma/store/kyma-store.module.d.ts
+++ src/kyma/store/kyma-store.module.d.ts
@@ -1,13 +1,4 @@
 import { StateConfig } from '../../state/config/state-config';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '../../state/state.module';
-import * as ɵngcc3 from '@ngrx/store';
-import * as ɵngcc4 from '@ngrx/effects';
 export declare function kymaStoreConfigFactory(): StateConfig;
 export declare class KymaStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<KymaStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.StateModule, typeof ɵngcc3.StoreFeatureModule, typeof ɵngcc4.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<KymaStoreModule>;
 }
-
-//# sourceMappingURL=kyma-store.module.d.ts.map
