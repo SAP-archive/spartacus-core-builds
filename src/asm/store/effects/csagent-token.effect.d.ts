898bfa40ea50dfc7a8752fede22223fa9d8e5020
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-10-20 19:34:17 +0000

    35e0ce929 Revert "feat: My Company E2E Skilet and Some Tests (#7845)"

--- src/asm/store/effects/csagent-token.effect.d.ts
+++ src/asm/store/effects/csagent-token.effect.d.ts
@@ -2,9 +2,14 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { UserAuthenticationTokenService } from '../../../auth/services/user-authentication/user-authentication-token.service';
 import { AsmActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class CustomerSupportAgentTokenEffects {
     private actions$;
     private userTokenService;
     loadCustomerSupportAgentToken$: Observable<AsmActions.CustomerSupportAgentTokenAction>;
     constructor(actions$: Actions, userTokenService: UserAuthenticationTokenService);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CustomerSupportAgentTokenEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<CustomerSupportAgentTokenEffects>;
 }
+
+//# sourceMappingURL=csagent-token.effect.d.ts.map
--- src/auth/auth.module.d.ts
+++ src/auth/auth.module.d.ts
@@ -1,4 +1,11 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from '@angular/common';
+import * as ɵngcc2 from './store/auth-store.module';
 export declare class AuthModule {
     static forRoot(): ModuleWithProviders<AuthModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AuthModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.AuthStoreModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<AuthModule>;
 }
+
+//# sourceMappingURL=auth.module.d.ts.map
