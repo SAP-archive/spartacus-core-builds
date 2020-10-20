70150ba909809376405b706ba1e48ec386000b0d
GitHub <noreply@github.com>
2020-10-20 20:49:57 +0000

    1e860ba85 Fix: cart-item-list handles null item list properly. (#8881)

--- src/asm/store/asm-store.module.d.ts
+++ src/asm/store/asm-store.module.d.ts
@@ -1,13 +1,4 @@
 import { StateConfig } from '../../state/config/state-config';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '../../state/state.module';
-import * as ɵngcc3 from '@ngrx/store';
-import * as ɵngcc4 from '@ngrx/effects';
 export declare function asmStoreConfigFactory(): StateConfig;
 export declare class AsmStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AsmStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.StateModule, typeof ɵngcc3.StoreFeatureModule, typeof ɵngcc4.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<AsmStoreModule>;
 }
-
-//# sourceMappingURL=asm-store.module.d.ts.map
