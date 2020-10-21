905e342b07cc610a4723623705d57242eea6639f
GitHub <noreply@github.com>
2020-10-21 23:19:06 +0000

    fb87e5ca7 fix: Facets null check (#9407)

--- src/process/store/process-store.module.d.ts
+++ src/process/store/process-store.module.d.ts
@@ -1,2 +1,9 @@
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from '../../state/state.module';
+import * as ɵngcc2 from '@ngrx/store';
 export declare class ProcessStoreModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProcessStoreModule, never, [typeof ɵngcc1.StateModule, typeof ɵngcc2.StoreFeatureModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProcessStoreModule>;
 }
+
+//# sourceMappingURL=process-store.module.d.ts.map
