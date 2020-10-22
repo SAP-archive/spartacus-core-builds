12186057ca748fa65e8b24e634710861710c635a
GitHub <noreply@github.com>
2020-10-22 01:06:02 +0000

    cb6409aa8 fix: Details page load action firing when navigating outside of the page (GH-9367)

--- src/cart/services/multi-cart-state-persistence.service.d.ts
+++ src/cart/services/multi-cart-state-persistence.service.d.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { SiteContextParamsService } from '../../site-context/services/site-context-params.service';
 import { StatePersistenceService } from '../../state/services/state-persistence.service';
 import { StateWithMultiCart } from '../store/multi-cart-state';
-import * as ɵngcc0 from '@angular/core';
 export declare class MultiCartStatePersistenceService {
     protected statePersistenceService: StatePersistenceService;
     protected store: Store<StateWithMultiCart>;
@@ -16,7 +15,4 @@ export declare class MultiCartStatePersistenceService {
     protected onRead(state: {
         active: string;
     }): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<MultiCartStatePersistenceService, never>;
 }
-
-//# sourceMappingURL=multi-cart-state-persistence.service.d.ts.map
--- src/user/store/effects/regions.effect.d.ts
+++ src/user/store/effects/regions.effect.d.ts
@@ -3,15 +3,10 @@ import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { SiteConnector } from '../../../site-context/connectors/site.connector';
 import { UserActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class RegionsEffects {
     private actions$;
     private siteConnector;
     loadRegions$: Observable<UserActions.RegionsAction>;
     resetRegions$: Observable<Action>;
     constructor(actions$: Actions, siteConnector: SiteConnector);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<RegionsEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<RegionsEffects>;
 }
-
-//# sourceMappingURL=regions.effect.d.ts.map
