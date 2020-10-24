4a3626aef68d3bc061b739812e0a5be585699c3e
GitHub <noreply@github.com>
2020-10-24 09:17:43 +0000

    e3cdb0dcb fix: Cypress configurations to let smoke/checkout-flow.e2e-spec.ts pass using normal config (GH-9477)

--- src/anonymous-consents/connectors/anonymous-consent-templates.connector.d.ts
+++ src/anonymous-consents/connectors/anonymous-consent-templates.connector.d.ts
@@ -1,9 +1,13 @@
 import { Observable } from 'rxjs';
 import { AnonymousConsent, ConsentTemplate } from '../../model/consent.model';
 import { AnonymousConsentTemplatesAdapter } from './anonymous-consent-templates.adapter';
+import * as ɵngcc0 from '@angular/core';
 export declare class AnonymousConsentTemplatesConnector {
     protected adapter: AnonymousConsentTemplatesAdapter;
     constructor(adapter: AnonymousConsentTemplatesAdapter);
     loadAnonymousConsentTemplates(): Observable<ConsentTemplate[]>;
     loadAnonymousConsents(): Observable<AnonymousConsent[]> | null;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<AnonymousConsentTemplatesConnector, never>;
 }
+
+//# sourceMappingURL=anonymous-consent-templates.connector.d.ts.map
