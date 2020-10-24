36df51de249a96cfc5e045a2fa5114477730c07a
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-10-24 06:38:58 +0000

    efaadda6f Adding netlify reference

--- src/occ/adapters/cart/occ-cart.adapter.d.ts
+++ src/occ/adapters/cart/occ-cart.adapter.d.ts
@@ -4,6 +4,7 @@ import { CartAdapter } from '../../../cart/connectors/cart/cart.adapter';
 import { Cart } from '../../../model/cart.model';
 import { ConverterService } from '../../../util/converter.service';
 import { OccEndpointsService } from '../../services/occ-endpoints.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class OccCartAdapter implements CartAdapter {
     protected http: HttpClient;
     protected occEndpointsService: OccEndpointsService;
@@ -14,4 +15,8 @@ export declare class OccCartAdapter implements CartAdapter {
     create(userId: string, oldCartId?: string, toMergeCartGuid?: string): Observable<Cart>;
     delete(userId: string, cartId: string): Observable<{}>;
     addEmail(userId: string, cartId: string, email: string): Observable<{}>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCartAdapter, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<OccCartAdapter>;
 }
+
+//# sourceMappingURL=occ-cart.adapter.d.ts.map
