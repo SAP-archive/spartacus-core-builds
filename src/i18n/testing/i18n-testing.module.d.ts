cf2b39be5555efef70a13b4a93629931d63b616f
GitHub <noreply@github.com>
2020-10-29 19:12:00 +0000

    388694e37 test: Order Approvals e2e tests (#8220)

--- src/i18n/testing/i18n-testing.module.d.ts
+++ src/i18n/testing/i18n-testing.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './mock-translate.pipe';
-import * as ɵngcc2 from './mock-date.pipe';
 export declare class I18nTestingModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<I18nTestingModule, [typeof ɵngcc1.MockTranslatePipe, typeof ɵngcc2.MockDatePipe], never, [typeof ɵngcc1.MockTranslatePipe, typeof ɵngcc2.MockDatePipe]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<I18nTestingModule>;
 }
-
-//# sourceMappingURL=i18n-testing.module.d.ts.map
