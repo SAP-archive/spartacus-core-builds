0feccdf4ff37ed82d86c2bd966487d7c585621a4
GitHub <noreply@github.com>
2020-10-30 21:22:52 +0000

    94670cf88 chore: added style lint for directionality (#9533)

--- src/asm/services/asm-auth.service.d.ts
+++ src/asm/services/asm-auth.service.d.ts
@@ -12,6 +12,7 @@ import { AsmAuthStorageService } from './asm-auth-storage.service';
  * Version of BasicAuthService that is working for both user na CS agent.
  * Overrides BasicAuthService when ASM module is enabled.
  */
+import * as ɵngcc0 from '@angular/core';
 export declare class AsmAuthService extends BasicAuthService {
     protected store: Store<StateWithClientAuth>;
     protected userIdService: UserIdService;
@@ -41,4 +42,7 @@ export declare class AsmAuthService extends BasicAuthService {
      * Returns `true` if user is logged in or being emulated.
      */
     isUserLoggedIn(): Observable<boolean>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<AsmAuthService, never>;
 }
+
+//# sourceMappingURL=asm-auth.service.d.ts.map
