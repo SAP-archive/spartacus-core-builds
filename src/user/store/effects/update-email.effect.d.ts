8920ed1b7bf6f1806f99d6a1d94b72abcc7f37b1
GitHub <noreply@github.com>
2020-11-02 22:59:48 +0000

    3aeb1d7de Fix: Resolved collapseMenu when invoked by svg dom element. (#8763)

--- src/user/store/effects/update-email.effect.d.ts
+++ src/user/store/effects/update-email.effect.d.ts
@@ -2,14 +2,9 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { UserConnector } from '../../connectors/user/user.connector';
 import { UserActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class UpdateEmailEffects {
     private actions$;
     private userAccountConnector;
     constructor(actions$: Actions, userAccountConnector: UserConnector);
     updateEmail$: Observable<UserActions.UpdateEmailSuccessAction | UserActions.UpdateEmailErrorAction>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UpdateEmailEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<UpdateEmailEffects>;
 }
-
-//# sourceMappingURL=update-email.effect.d.ts.map
