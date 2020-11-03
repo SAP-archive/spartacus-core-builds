4f96adbaf186f69a9d50d0d3662df5071a807f5a
GitHub <noreply@github.com>
2020-11-03 12:05:04 +0000

    087b40d30 feat Org Unit E2E Assignment Tests (#9552)

--- src/site-context/store/effects/base-site.effect.d.ts
+++ src/site-context/store/effects/base-site.effect.d.ts
@@ -2,14 +2,9 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { SiteConnector } from '../../connectors/site.connector';
 import { SiteContextActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class BaseSiteEffects {
     private actions$;
     private siteConnector;
     loadBaseSite$: Observable<SiteContextActions.LoadBaseSiteSuccess | SiteContextActions.LoadBaseSiteFail>;
     constructor(actions$: Actions, siteConnector: SiteConnector);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<BaseSiteEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<BaseSiteEffects>;
 }
-
-//# sourceMappingURL=base-site.effect.d.ts.map
