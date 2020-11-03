8684caf92271956b7192db17b206b461ee30deb4
GitHub <noreply@github.com>
2020-11-03 17:54:44 +0000

    17b426f92 feat: schematics - support order-approvals feature (#9619)

--- src/smart-edit/smart-edit.module.d.ts
+++ src/smart-edit/smart-edit.module.d.ts
@@ -1,9 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
 export declare class SmartEditModule {
     static forRoot(): ModuleWithProviders<SmartEditModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<SmartEditModule, never, never, never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<SmartEditModule>;
 }
-
-//# sourceMappingURL=smart-edit.module.d.ts.map
