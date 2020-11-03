a44237bb595426297af4851cce5aec5bc1a85cf8
GitHub <noreply@github.com>
2020-11-03 20:15:06 +0000

    2a07eedfc Fix: Product attributes page now displays unit symbol with value if present. (#8845) (#9463)

--- src/user/store/effects/user-orders.effect.d.ts
+++ src/user/store/effects/user-orders.effect.d.ts
@@ -2,7 +2,6 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { UserOrderConnector, UserReplenishmentOrderConnector } from '../../connectors/index';
 import { UserActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class UserOrdersEffect {
     private actions$;
     private orderConnector;
@@ -10,8 +9,4 @@ export declare class UserOrdersEffect {
     constructor(actions$: Actions, orderConnector: UserOrderConnector, replenishmentOrderConnector: UserReplenishmentOrderConnector);
     loadUserOrders$: Observable<UserActions.UserOrdersAction>;
     resetUserOrders$: Observable<UserActions.ClearUserOrders>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserOrdersEffect, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<UserOrdersEffect>;
 }
-
-//# sourceMappingURL=user-orders.effect.d.ts.map
