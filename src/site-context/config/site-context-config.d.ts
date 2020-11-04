bf2cfd52c077d80eef8fb31a813eefef593c2d58
GitHub <noreply@github.com>
2020-11-04 17:13:11 +0000

    8b41a8dae migration 3.0: AddedToCartDialogComponent and CartItemListComponent (#9653)

--- src/site-context/config/site-context-config.d.ts
+++ src/site-context/config/site-context-config.d.ts
@@ -1,6 +1,10 @@
+import * as ɵngcc0 from '@angular/core';
 export declare abstract class SiteContextConfig {
     context?: {
         urlParameters?: string[];
         [contextName: string]: string[];
     };
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<SiteContextConfig, never>;
 }
+
+//# sourceMappingURL=site-context-config.d.ts.map
