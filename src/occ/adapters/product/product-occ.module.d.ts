8c601ca474bf54ac0595be2eeedf02c3bb0f8230
GitHub <noreply@github.com>
2020-11-05 15:44:18 +0000

    04ff18c2e chore: migration 3.0 - CartItemComponent (#9654)

--- src/occ/adapters/product/product-occ.module.d.ts
+++ src/occ/adapters/product/product-occ.module.d.ts
@@ -1,9 +1,3 @@
 import './product-occ-config';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
 export declare class ProductOccModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductOccModule, never, [typeof ɵngcc1.CommonModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductOccModule>;
 }
-
-//# sourceMappingURL=product-occ.module.d.ts.map
