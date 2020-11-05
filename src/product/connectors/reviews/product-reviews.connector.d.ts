0a38cd090e68e4e1596d1f0040e46bbfbc9e3950
GitHub <noreply@github.com>
2020-11-05 12:46:10 +0000

    2b7ed274e chore: migration 3.0 - document typo fix in translation key 'managementInMyAccount' (#9658)

--- src/product/connectors/reviews/product-reviews.connector.d.ts
+++ src/product/connectors/reviews/product-reviews.connector.d.ts
@@ -1,13 +1,9 @@
 import { Observable } from 'rxjs';
 import { ProductReviewsAdapter } from './product-reviews.adapter';
 import { Review } from '../../../model/product.model';
-import * as ɵngcc0 from '@angular/core';
 export declare class ProductReviewsConnector {
     protected adapter: ProductReviewsAdapter;
     constructor(adapter: ProductReviewsAdapter);
     get(productCode: string, maxCount?: number): Observable<Review[]>;
     add(productCode: string, review: any): Observable<Review>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ProductReviewsConnector, never>;
 }
-
-//# sourceMappingURL=product-reviews.connector.d.ts.map
