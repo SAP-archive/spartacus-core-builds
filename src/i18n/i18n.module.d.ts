db58a35d1fa06fdb0c7cc55c01805f26e932fbd9
GitHub <noreply@github.com>
2020-11-06 21:22:53 +0000

    fdc02a983 chore: breakpoint service deprecation migration (#9669)

--- src/i18n/i18n.module.d.ts
+++ src/i18n/i18n.module.d.ts
@@ -1,4 +1,11 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './translate.pipe';
+import * as ɵngcc2 from './date.pipe';
 export declare class I18nModule {
     static forRoot(): ModuleWithProviders<I18nModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<I18nModule, [typeof ɵngcc1.TranslatePipe, typeof ɵngcc2.CxDatePipe], never, [typeof ɵngcc1.TranslatePipe, typeof ɵngcc2.CxDatePipe]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<I18nModule>;
 }
+
+//# sourceMappingURL=i18n.module.d.ts.map
