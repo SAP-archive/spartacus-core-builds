5e7e729b000f0303f42beb31f12969a4d04be451
GitHub <noreply@github.com>
2020-11-09 16:45:55 +0000

    296fa04ac feat: Add specific selectors to all organization table-cell components #9672 (#9685)

--- src/site-context/store/effects/base-site.effect.d.ts
+++ src/site-context/store/effects/base-site.effect.d.ts
@@ -2,10 +2,15 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { SiteConnector } from '../../connectors/site.connector';
 import { SiteContextActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class BaseSiteEffects {
     private actions$;
     private siteConnector;
     loadBaseSite$: Observable<SiteContextActions.LoadBaseSiteSuccess | SiteContextActions.LoadBaseSiteFail>;
     loadBaseSites$: Observable<SiteContextActions.LoadBaseSitesSuccess | SiteContextActions.LoadBaseSitesFail>;
     constructor(actions$: Actions, siteConnector: SiteConnector);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<BaseSiteEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<BaseSiteEffects>;
 }
+
+//# sourceMappingURL=base-site.effect.d.ts.map
