fa8df527da6db0b5a59d570cefd864eb87c23642
GitHub <noreply@github.com>
2020-11-09 15:18:52 +0000

    b30942148 chore: Added ctor deprecation for multi-cart service (#9684)

--- src/cart/connectors/voucher/cart-voucher.connector.d.ts
+++ src/cart/connectors/voucher/cart-voucher.connector.d.ts
@@ -1,12 +1,8 @@
 import { Observable } from 'rxjs';
 import { CartVoucherAdapter } from './cart-voucher.adapter';
-import * as ɵngcc0 from '@angular/core';
 export declare class CartVoucherConnector {
     protected adapter: CartVoucherAdapter;
     constructor(adapter: CartVoucherAdapter);
     add(userId: string, cartId: string, voucherId: string): Observable<{}>;
     remove(userId: string, cartId: string, voucherId: string): Observable<{}>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CartVoucherConnector, never>;
 }
-
-//# sourceMappingURL=cart-voucher.connector.d.ts.map
--- src/occ/adapters/checkout/occ-checkout-delivery.adapter.d.ts
+++ src/occ/adapters/checkout/occ-checkout-delivery.adapter.d.ts
@@ -5,7 +5,6 @@ import { Address } from '../../../model/address.model';
 import { DeliveryMode } from '../../../model/order.model';
 import { ConverterService } from '../../../util/converter.service';
 import { OccEndpointsService } from '../../services/occ-endpoints.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class OccCheckoutDeliveryAdapter implements CheckoutDeliveryAdapter {
     protected http: HttpClient;
     protected occEndpoints: OccEndpointsService;
@@ -17,8 +16,4 @@ export declare class OccCheckoutDeliveryAdapter implements CheckoutDeliveryAdapt
     setMode(userId: string, cartId: string, deliveryModeId: string): Observable<any>;
     getMode(userId: string, cartId: string): Observable<any>;
     getSupportedModes(userId: string, cartId: string): Observable<DeliveryMode[]>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCheckoutDeliveryAdapter, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<OccCheckoutDeliveryAdapter>;
 }
-
-//# sourceMappingURL=occ-checkout-delivery.adapter.d.ts.map
