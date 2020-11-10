ca134559e89ed6c4cfa78999cfd9fa862f436e37
GitHub <noreply@github.com>
2020-11-10 14:21:31 +0000

    91a0f653b fix: Deferred loading causes flickering with SSR (#9678)

--- src/store-finder/store/effects/view-all-stores.effect.d.ts
+++ src/store-finder/store/effects/view-all-stores.effect.d.ts
@@ -2,9 +2,14 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { StoreFinderConnector } from '../../connectors/store-finder.connector';
 import { StoreFinderActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class ViewAllStoresEffect {
     private actions$;
     private storeFinderConnector;
     constructor(actions$: Actions, storeFinderConnector: StoreFinderConnector);
     viewAllStores$: Observable<StoreFinderActions.ViewAllStoresSuccess | StoreFinderActions.ViewAllStoresFail>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<ViewAllStoresEffect, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<ViewAllStoresEffect>;
 }
+
+//# sourceMappingURL=view-all-stores.effect.d.ts.map
