9232736ee3aae4d72074960c2c1b5aba8d68df4c
GitHub <noreply@github.com>
2020-11-10 16:31:52 +0000

    e0d81007d Migration for modified public method of StorefrontComponent. (#9699)

--- src/auth/client-auth/http-interceptors/client-token.interceptor.d.ts
+++ src/auth/client-auth/http-interceptors/client-token.interceptor.d.ts
@@ -8,7 +8,6 @@ import { ClientTokenService } from '../services/client-token.service';
  * Interceptor for handling requests with `USE_CLIENT_TOKEN` header.
  * Provides `Authorization` header with client token and handles errors related to client auth.
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class ClientTokenInterceptor implements HttpInterceptor {
     protected clientTokenService: ClientTokenService;
     protected clientErrorHandlingService: ClientErrorHandlingService;
@@ -18,7 +17,4 @@ export declare class ClientTokenInterceptor implements HttpInterceptor {
     protected getClientToken(isClientTokenRequest: boolean): Observable<ClientToken>;
     protected isClientTokenRequest(request: HttpRequest<any>): boolean;
     protected isExpiredToken(resp: HttpErrorResponse): boolean;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ClientTokenInterceptor, never>;
 }
-
-//# sourceMappingURL=client-token.interceptor.d.ts.map
