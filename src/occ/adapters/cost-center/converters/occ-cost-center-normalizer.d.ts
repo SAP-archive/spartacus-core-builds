007213c8a6f74f5b5bcf497c628d9779de1e9eda
GitHub <noreply@github.com>
2020-11-10 19:16:33 +0000

    b3a6f58bc chore: Breaking change in product reference component from #8223 (GH-9696)

--- src/occ/adapters/cost-center/converters/occ-cost-center-normalizer.d.ts
+++ src/occ/adapters/cost-center/converters/occ-cost-center-normalizer.d.ts
@@ -1,7 +1,6 @@
 import { CostCenter } from '../../../../model/org-unit.model';
 import { Converter } from '../../../../util/converter.service';
 import { Occ } from '../../../occ-models/occ.models';
-import * as ɵngcc0 from '@angular/core';
 export declare class OccCostCenterNormalizer implements Converter<Occ.CostCenter, CostCenter> {
     convert(source: Occ.CostCenter, target?: CostCenter): CostCenter;
     /**
@@ -9,8 +8,4 @@ export declare class OccCostCenterNormalizer implements Converter<Occ.CostCenter
      * to be of type boolean.
      */
     protected normalizeBoolean(property: string | boolean): boolean;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCostCenterNormalizer, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<OccCostCenterNormalizer>;
 }
-
-//# sourceMappingURL=occ-cost-center-normalizer.d.ts.map
