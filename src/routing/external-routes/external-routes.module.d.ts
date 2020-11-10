926bfe5d771feb0a0f5643ae1c7c18eab3ec6844
GitHub <noreply@github.com>
2020-11-10 16:51:28 +0000

    023120686 Fix: Added label for out of stock.(#8923). (#9661)

--- src/routing/external-routes/external-routes.module.d.ts
+++ src/routing/external-routes/external-routes.module.d.ts
@@ -2,6 +2,11 @@ import { ModuleWithProviders } from '@angular/core';
 /**
  * Prepends the external route that redirects to a different storefront system for configured URLs
  */
+import * as ɵngcc0 from '@angular/core';
 export declare class ExternalRoutesModule {
     static forRoot(): ModuleWithProviders<ExternalRoutesModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ExternalRoutesModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ExternalRoutesModule>;
 }
+
+//# sourceMappingURL=external-routes.module.d.ts.map
