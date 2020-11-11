7a338d7014ca469f9053d4585d83ddb5ac01a12d
GitHub <noreply@github.com>
2020-11-11 04:16:06 +0000

    7508f8f78 fix: Rejecting an order without a comment does not close the approval form anymore. (GH-9731)(#9735)

--- src/anonymous-consents/store/effects/anonymous-consents.effect.d.ts
+++ src/anonymous-consents/store/effects/anonymous-consents.effect.d.ts
@@ -7,6 +7,7 @@ import { AnonymousConsentsConfig } from '../../config/anonymous-consents-config'
 import { AnonymousConsentTemplatesConnector } from '../../connectors/anonymous-consent-templates.connector';
 import { AnonymousConsentsService } from '../../facade/index';
 import { AnonymousConsentsActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class AnonymousConsentsEffects {
     private actions$;
     private anonymousConsentTemplatesConnector;
@@ -28,4 +29,8 @@ export declare class AnonymousConsentsEffects {
      * @param newVersions versions of the new consents
      */
     private detectUpdatedVersion;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<AnonymousConsentsEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<AnonymousConsentsEffects>;
 }
+
+//# sourceMappingURL=anonymous-consents.effect.d.ts.map
