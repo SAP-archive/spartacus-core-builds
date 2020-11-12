b2a24ad3aa41d039c501249ddc80dfd195c9445a
GitHub <noreply@github.com>
2020-11-12 12:27:00 +0000

    fb8b74d55 chore: migration for StockNotificationComponent (#9730)

--- src/occ/adapters/cost-center/cost-center-occ.module.d.ts
+++ src/occ/adapters/cost-center/cost-center-occ.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '../../../config/config.module';
 export declare class CostCenterOccModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CostCenterOccModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.ConfigModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CostCenterOccModule>;
 }
-
-//# sourceMappingURL=cost-center-occ.module.d.ts.map
