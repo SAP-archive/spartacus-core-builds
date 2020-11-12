eaf0321fd581339cb42793414a6e4389fb5b02c4
GitHub <noreply@github.com>
2020-11-12 11:39:20 +0000

    217354e3b chore: migration for removed properties of LoginFormComponent and removed redirect behavior (#9732)

--- src/occ/adapters/store-finder/store-finder-occ.module.d.ts
+++ src/occ/adapters/store-finder/store-finder-occ.module.d.ts
@@ -1,2 +1,7 @@
+import * as ɵngcc0 from '@angular/core';
 export declare class StoreFinderOccModule {
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StoreFinderOccModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<StoreFinderOccModule>;
 }
+
+//# sourceMappingURL=store-finder-occ.module.d.ts.map
