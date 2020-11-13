24de6b9d9f20553ade8ded19c8ed96c9403e0c90
GitHub <noreply@github.com>
2020-11-13 15:37:13 +0000

    0cb1f40f2 fix: Approvers list changes based on what list you checked before #9465 (#9761)

--- src/smart-edit/http-interceptors/cms-ticket.interceptor.d.ts
+++ src/smart-edit/http-interceptors/cms-ticket.interceptor.d.ts
@@ -1,12 +1,8 @@
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SmartEditService } from '../services/smart-edit.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class CmsTicketInterceptor implements HttpInterceptor {
     private service;
     constructor(service: SmartEditService);
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CmsTicketInterceptor, never>;
 }
-
-//# sourceMappingURL=cms-ticket.interceptor.d.ts.map
