0b3453c11a8923c5d32040c09400a461e4a0b89d
GitHub <noreply@github.com>
2020-11-13 11:35:31 +0000

    d0bb621c1 chore: Migrations for AuthModule, AsmModule, KymaModule and CDC. (#9744)

--- src/user/store/effects/user-details.effect.d.ts
+++ src/user/store/effects/user-details.effect.d.ts
@@ -2,15 +2,10 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { UserConnector } from '../../connectors/user/user.connector';
 import { UserActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class UserDetailsEffects {
     private actions$;
     private userConnector;
     loadUserDetails$: Observable<UserActions.UserDetailsAction>;
     updateUserDetails$: Observable<UserActions.UpdateUserDetailsSuccess | UserActions.UpdateUserDetailsFail>;
     constructor(actions$: Actions, userConnector: UserConnector);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserDetailsEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<UserDetailsEffects>;
 }
-
-//# sourceMappingURL=user-details.effect.d.ts.map
