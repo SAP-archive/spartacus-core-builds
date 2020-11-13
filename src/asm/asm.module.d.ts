c118ad3e483d7406448e1e777236ee84d7a1bce2
GitHub <noreply@github.com>
2020-11-13 12:14:43 +0000

    34e8abf41 Auth/session management deprecations (#9752)

--- src/asm/asm.module.d.ts
+++ src/asm/asm.module.d.ts
@@ -1,13 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 import { AsmStatePersistenceService } from './services/asm-state-persistence.service';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from './store/asm-store.module';
 export declare function asmStatePersistenceFactory(asmStatePersistenceService: AsmStatePersistenceService): () => void;
 export declare class AsmModule {
     static forRoot(): ModuleWithProviders<AsmModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AsmModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.AsmStoreModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<AsmModule>;
 }
-
-//# sourceMappingURL=asm.module.d.ts.map
