38576a940624b06259d93971d2d3a3692f52b640
GitHub <noreply@github.com>
2020-11-13 16:39:26 +0000

    65c490b20 chore: Auth/session management deprecations - guards (GH-9711)

--- src/site-context/store/effects/languages.effect.d.ts
+++ src/site-context/store/effects/languages.effect.d.ts
@@ -5,6 +5,7 @@ import { WindowRef } from '../../../window/window-ref';
 import { SiteConnector } from '../../connectors/site.connector';
 import { SiteContextActions } from '../actions/index';
 import { StateWithSiteContext } from '../state';
+import * as ɵngcc0 from '@angular/core';
 export declare class LanguagesEffects {
     private actions$;
     private siteConnector;
@@ -14,4 +15,8 @@ export declare class LanguagesEffects {
     persist$: Observable<void>;
     activateLanguage$: Observable<SiteContextActions.LanguageChange>;
     constructor(actions$: Actions, siteConnector: SiteConnector, winRef: WindowRef, state: Store<StateWithSiteContext>);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<LanguagesEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<LanguagesEffects>;
 }
+
+//# sourceMappingURL=languages.effect.d.ts.map
