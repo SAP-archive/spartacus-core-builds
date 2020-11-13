b7ec44b46d7c93f28edaddc15b109c96b62c7284
GitHub <noreply@github.com>
2020-11-13 14:59:40 +0000

    4f8deb5e1 feat: inform profiletag when a consent has been revoked. (GH-9474)

--- src/product/store/product-store.module.d.ts
+++ src/product/store/product-store.module.d.ts
@@ -1,12 +1,4 @@
 import { StateConfig } from '../../state/config/state-config';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from '@ngrx/store';
-import * as ɵngcc3 from '@ngrx/effects';
 export declare function productStoreConfigFactory(): StateConfig;
 export declare class ProductStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductStoreModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.StoreFeatureModule, typeof ɵngcc3.EffectsFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductStoreModule>;
 }
-
-//# sourceMappingURL=product-store.module.d.ts.map
