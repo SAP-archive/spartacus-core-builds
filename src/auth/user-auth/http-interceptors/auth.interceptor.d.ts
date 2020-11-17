1cd65e8ad1b42509564c75527231004ea706ff33
GitHub <noreply@github.com>
2020-11-17 10:50:59 +0000

    5a12605c4 feat: Guard non existing and existing but not active B2B entity items (#8973)

--- src/auth/user-auth/http-interceptors/auth.interceptor.d.ts
+++ src/auth/user-auth/http-interceptors/auth.interceptor.d.ts
@@ -6,14 +6,10 @@ import { AuthHttpHeaderService } from '../services/auth-http-header.service';
  * Responsible for catching auth errors and providing `Authorization` header for API calls.
  * Uses AuthHttpHeaderService for request manipulation and error handling. Interceptor only hooks into request send/received events.
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class AuthInterceptor implements HttpInterceptor {
     protected authHttpHeaderService: AuthHttpHeaderService;
     protected authConfigService: AuthConfigService;
     constructor(authHttpHeaderService: AuthHttpHeaderService, authConfigService: AuthConfigService);
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
     protected isExpiredToken(resp: HttpErrorResponse): boolean;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AuthInterceptor, never>;
 }
-
-//# sourceMappingURL=auth.interceptor.d.ts.map
--- src/user/connectors/order/user-order.connector.d.ts
+++ src/user/connectors/order/user-order.connector.d.ts
@@ -2,7 +2,6 @@ import { Observable } from 'rxjs';
 import { ConsignmentTracking } from '../../../model/consignment-tracking.model';
 import { CancellationRequestEntryInputList, Order, OrderHistoryList, ReturnRequest, ReturnRequestEntryInputList, ReturnRequestList, ReturnRequestModification } from '../../../model/order.model';
 import { UserOrderAdapter } from './user-order.adapter';
-import * as ɵngcc0 from '@angular/core';
 export declare class UserOrderConnector {
     protected adapter: UserOrderAdapter;
     constructor(adapter: UserOrderAdapter);
@@ -14,7 +13,4 @@ export declare class UserOrderConnector {
     getReturnRequestDetail(userId: string, returnRequestCode: string): Observable<ReturnRequest>;
     getReturnRequestList(userId: string, pageSize?: number, currentPage?: number, sort?: string): Observable<ReturnRequestList>;
     cancelReturnRequest(userId: string, returnRequestCode: string, returnRequestModification: ReturnRequestModification): Observable<{}>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserOrderConnector, never>;
 }
-
-//# sourceMappingURL=user-order.connector.d.ts.map
