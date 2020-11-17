c9859a9349c7368904cb8052f7821278b96331f9
GitHub <noreply@github.com>
2020-11-17 12:57:09 +0000

    569ceb0c8 chore: Schematics update for SSR optimizations (#9757)

--- src/user/connectors/address/user-address.connector.d.ts
+++ src/user/connectors/address/user-address.connector.d.ts
@@ -1,6 +1,7 @@
 import { UserAddressAdapter } from './user-address.adapter';
 import { Observable } from 'rxjs';
 import { Address, AddressValidation } from '../../../model/address.model';
+import * as ɵngcc0 from '@angular/core';
 export declare class UserAddressConnector {
     protected adapter: UserAddressAdapter;
     constructor(adapter: UserAddressAdapter);
@@ -9,4 +10,7 @@ export declare class UserAddressConnector {
     update(userId: string, addressId: string, address: Address): Observable<{}>;
     verify(userId: string, address: Address): Observable<AddressValidation>;
     delete(userId: string, addressId: string): Observable<{}>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserAddressConnector, never>;
 }
+
+//# sourceMappingURL=user-address.connector.d.ts.map
