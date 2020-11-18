6f638e1476244e745d8740ee88262e1e5fcb240f
GitHub <noreply@github.com>
2020-11-18 20:13:41 +0000

    0c73bc555 fix: organization schematics - replace B2cStorefrontModule with B2bStorefrontModule (#9836)

--- src/user/store/effects/customer-coupon.effect.d.ts
+++ src/user/store/effects/customer-coupon.effect.d.ts
@@ -2,7 +2,6 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import * as fromCustomerCouponsAction from '../actions/customer-coupon.action';
 import { CustomerCouponConnector } from '../../connectors/customer-coupon/customer-coupon.connector';
-import * as ɵngcc0 from '@angular/core';
 export declare class CustomerCouponEffects {
     private actions$;
     private customerCouponConnector;
@@ -11,8 +10,4 @@ export declare class CustomerCouponEffects {
     unsubscribeCustomerCoupon$: Observable<fromCustomerCouponsAction.CustomerCouponAction>;
     claimCustomerCoupon$: Observable<fromCustomerCouponsAction.CustomerCouponAction>;
     constructor(actions$: Actions, customerCouponConnector: CustomerCouponConnector);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CustomerCouponEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<CustomerCouponEffects>;
 }
-
-//# sourceMappingURL=customer-coupon.effect.d.ts.map
