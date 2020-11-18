f069c727d404b1d18abeeab3516a97856ed78870
GitHub <noreply@github.com>
2020-11-18 11:26:24 +0000

    b59cdd248 feat: refactor - prefix all organization/administration components' selectors with cx-org #9484 (#9826)

--- src/config/config-initializer/config-initializer.module.d.ts
+++ src/config/config-initializer/config-initializer.module.d.ts
@@ -1,8 +1,13 @@
 import { ModuleWithProviders } from '@angular/core';
 import { ConfigInitializerService } from './config-initializer.service';
 import { ConfigInitializer } from './config-initializer';
+import * as ɵngcc0 from '@angular/core';
 export declare function configInitializerFactory(configInitializer: ConfigInitializerService, initializers: ConfigInitializer[]): () => Promise<void>;
 export declare function locationInitializedFactory(configInitializer: ConfigInitializerService): Promise<any>;
 export declare class ConfigInitializerModule {
     static forRoot(): ModuleWithProviders<ConfigInitializerModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ConfigInitializerModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ConfigInitializerModule>;
 }
+
+//# sourceMappingURL=config-initializer.module.d.ts.map
