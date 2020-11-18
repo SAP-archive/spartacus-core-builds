3af8d1eafa4906b9c2301bb0eaf1851885492b6d
GitHub <noreply@github.com>
2020-11-18 18:43:44 +0000

    a0bd99e00 chore: Update package scripts to run CDS in our pipeline (GH-9912)

--- src/cms/store/effects/components.effect.d.ts
+++ src/cms/store/effects/components.effect.d.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { CmsComponent } from '../../../model/cms.model';
 import { CmsComponentConnector } from '../../connectors/component/cms-component.connector';
 import { CmsActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class ComponentsEffects {
     private actions$;
     private cmsComponentConnector;
@@ -10,4 +11,8 @@ export declare class ComponentsEffects {
     private contextChange$;
     loadComponent$: (({ scheduler, debounce }?: any) => Observable<CmsActions.LoadCmsComponentSuccess<CmsComponent> | CmsActions.LoadCmsComponentFail>) & import("@ngrx/effects").CreateEffectMetadata;
     private loadComponentsEffect;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<ComponentsEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<ComponentsEffects>;
 }
+
+//# sourceMappingURL=components.effect.d.ts.map
