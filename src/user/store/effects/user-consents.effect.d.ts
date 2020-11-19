9876959d479f2fcd8cfc55e62e59bb75eb458b8c
GitHub <noreply@github.com>
2020-11-19 11:44:48 +0000

    ed8a3ef43 feat: Improve combining injectors for lazy loaded modules (#9933)

--- src/user/store/effects/user-consents.effect.d.ts
+++ src/user/store/effects/user-consents.effect.d.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { GlobalMessageActions } from '../../../global-message/store/actions';
 import { UserConsentConnector } from '../../connectors/consent/user-consent.connector';
 import { UserActions } from '../actions/index';
-import * as ɵngcc0 from '@angular/core';
 export declare class UserConsentsEffect {
     private actions$;
     private userConsentConnector;
@@ -12,8 +11,4 @@ export declare class UserConsentsEffect {
     giveConsent$: Observable<UserActions.UserConsentsAction | GlobalMessageActions.RemoveMessagesByType>;
     withdrawConsent$: Observable<UserActions.UserConsentsAction>;
     constructor(actions$: Actions, userConsentConnector: UserConsentConnector);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserConsentsEffect, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<UserConsentsEffect>;
 }
-
-//# sourceMappingURL=user-consents.effect.d.ts.map
