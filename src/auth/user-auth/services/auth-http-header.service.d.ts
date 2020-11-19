443f0fc3080dea4e3cf6e29ab7aeed81df0e6559
GitHub <noreply@github.com>
2020-11-19 14:23:27 +0000

    f45d0837a chore: Fixes for storefinder migrations (#9945)

--- src/auth/user-auth/services/auth-http-header.service.d.ts
+++ src/auth/user-auth/services/auth-http-header.service.d.ts
@@ -10,7 +10,6 @@ import { OAuthLibWrapperService } from './oauth-lib-wrapper.service';
 /**
  * Extendable service for `AuthInterceptor`.
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class AuthHttpHeaderService {
     protected authService: AuthService;
     protected authStorageService: AuthStorageService;
@@ -48,7 +47,4 @@ export declare class AuthHttpHeaderService {
      */
     protected handleExpiredToken(): Observable<AuthToken>;
     protected createNewRequestWithNewToken(request: HttpRequest<any>, token: AuthToken): HttpRequest<any>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AuthHttpHeaderService, never>;
 }
-
-//# sourceMappingURL=auth-http-header.service.d.ts.map
