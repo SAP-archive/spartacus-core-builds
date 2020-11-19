76337a373836c44ad030b408a41a77574912fc3e
GitHub <noreply@github.com>
2020-11-19 19:57:55 +0000

    60508a9d0 chore: ViewComponent deprecations (#9950)

--- src/occ/adapters/user/converters/occ-address-list-normalizer.d.ts
+++ src/occ/adapters/user/converters/occ-address-list-normalizer.d.ts
@@ -2,13 +2,8 @@ import { Address } from '../../../../model/address.model';
 import { EntitiesModel } from '../../../../model/misc.model';
 import { Converter, ConverterService } from '../../../../util/converter.service';
 import { Occ } from '../../../occ-models/occ.models';
-import * as ɵngcc0 from '@angular/core';
 export declare class OccAddressListNormalizer implements Converter<Occ.AddressList, EntitiesModel<Address>> {
     private converter;
     constructor(converter: ConverterService);
     convert(source: Occ.AddressList, target?: EntitiesModel<Address>): EntitiesModel<Address>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccAddressListNormalizer, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<OccAddressListNormalizer>;
 }
-
-//# sourceMappingURL=occ-address-list-normalizer.d.ts.map
