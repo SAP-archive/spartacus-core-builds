2762b3b72c5c031ef80ba419095c3ed3d9c13259
GitHub <noreply@github.com>
2020-11-19 14:09:31 +0000

    f45d0837a chore: Fixes for storefinder migrations (#9945)

--- src/user/store/effects/titles.effect.d.ts
+++ src/user/store/effects/titles.effect.d.ts
@@ -2,9 +2,14 @@ import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { UserConnector } from '../../connectors/user/user.connector';
 import { UserActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class TitlesEffects {
     private actions$;
     private userAccountConnector;
     loadTitles$: Observable<UserActions.TitlesAction>;
     constructor(actions$: Actions, userAccountConnector: UserConnector);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<TitlesEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<TitlesEffects>;
 }
+
+//# sourceMappingURL=titles.effect.d.ts.map
