e24ac143a70c5b4eab45375ded9312d60a556754
GitHub <noreply@github.com>
2020-11-19 18:37:04 +0000

    2f60e709d chore: reorder params in checkout guard ctor and add CheckoutGuard to migration log (#9902)

--- src/cms/connectors/page/cms-page.connector.d.ts
+++ src/cms/connectors/page/cms-page.connector.d.ts
@@ -3,7 +3,6 @@ import { PageContext } from '../../../routing/models/page-context.model';
 import { CmsStructureModel } from '../../model/page.model';
 import { CmsStructureConfigService } from '../../services/cms-structure-config.service';
 import { CmsPageAdapter } from './cms-page.adapter';
-import * as ɵngcc0 from '@angular/core';
 export declare class CmsPageConnector {
     protected cmsPageAdapter: CmsPageAdapter;
     protected cmsStructureConfigService: CmsStructureConfigService;
@@ -21,7 +20,4 @@ export declare class CmsPageConnector {
      * fine-grained CMS setup.
      */
     private mergeDefaultPageStructure;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CmsPageConnector, never>;
 }
-
-//# sourceMappingURL=cms-page.connector.d.ts.map
