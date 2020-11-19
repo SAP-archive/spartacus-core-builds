58c7e10d93efd74291514899475008d7c2313b2c
GitHub <noreply@github.com>
2020-11-19 12:46:13 +0000

    4a23fa4b8 chore: 3.0 migrations and docs (#9942)

--- src/checkout/facade/checkout-delivery.service.d.ts
+++ src/checkout/facade/checkout-delivery.service.d.ts
@@ -7,7 +7,6 @@ import { DeliveryMode } from '../../model/order.model';
 import { StateWithProcess } from '../../process/store/process-state';
 import { LoaderState } from '../../state/utils/loader/loader-state';
 import { StateWithCheckout } from '../store/checkout-state';
-import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutDeliveryService {
     protected checkoutStore: Store<StateWithCheckout | StateWithProcess<void>>;
     protected activeCartService: ActiveCartService;
@@ -102,7 +101,4 @@ export declare class CheckoutDeliveryService {
      */
     clearCheckoutDeliveryDetails(): void;
     protected actionAllowed(): boolean;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutDeliveryService, never>;
 }
-
-//# sourceMappingURL=checkout-delivery.service.d.ts.map
