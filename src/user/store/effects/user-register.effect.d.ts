7fd503aeb82540afbbfc14271b70f4d4c8955297
GitHub <noreply@github.com>
2020-11-20 07:25:16 +0000

    73e2be4ec chore: Migration inprovements (#9956)

--- src/user/store/effects/user-register.effect.d.ts
+++ src/user/store/effects/user-register.effect.d.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { AuthService } from '../../../auth/user-auth/facade/auth.service';
 import { UserConnector } from '../../connectors/user/user.connector';
 import { UserActions } from '../actions/index';
+import * as ɵngcc0 from '@angular/core';
 export declare class UserRegisterEffects {
     private actions$;
     private userConnector;
@@ -11,4 +12,8 @@ export declare class UserRegisterEffects {
     registerGuest$: Observable<UserActions.UserRegisterOrRemoveAction>;
     removeUser$: Observable<UserActions.UserRegisterOrRemoveAction>;
     constructor(actions$: Actions, userConnector: UserConnector, authService: AuthService);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<UserRegisterEffects, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<UserRegisterEffects>;
 }
+
+//# sourceMappingURL=user-register.effect.d.ts.map
