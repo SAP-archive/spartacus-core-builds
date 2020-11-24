61b4911dbb959658538e815856d3d8c9b0ce7745
GitHub <noreply@github.com>
2020-11-24 11:21:35 +0000

    660152d13 Overload method for stricter type checking (#9971)

--- src/i18n/date.pipe.d.ts
+++ src/i18n/date.pipe.d.ts
@@ -1,11 +1,17 @@
-import { PipeTransform } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { PipeTransform } from '@angular/core';
 import { LanguageService } from '../site-context/facade/language.service';
+import * as ɵngcc0 from '@angular/core';
 export declare class CxDatePipe extends DatePipe implements PipeTransform {
     protected language: LanguageService;
     constructor(language: LanguageService);
     transform(value: any, format?: string, timezone?: string): string | null;
+    transform(value: null | undefined, format?: string, timezone?: string): null;
     private getLang;
     private getActiveLang;
     private reportMissingLocaleData;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CxDatePipe, never>;
+    static ɵpipe: ɵngcc0.ɵɵPipeDefWithMeta<CxDatePipe, "cxDate">;
 }
+
+//# sourceMappingURL=date.pipe.d.ts.map
