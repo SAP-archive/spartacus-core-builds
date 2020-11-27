fe7e8a9f99083c2db307acb0d67ba148bd7d55d1
GitHub <noreply@github.com>
2020-11-27 09:22:55 +0000

    232a762a3 fix: render the correct product image format (#10023)

--- src/i18n/testing/mock-date.pipe.d.ts
+++ src/i18n/testing/mock-date.pipe.d.ts
@@ -1,5 +1,10 @@
 import { DatePipe } from '@angular/common';
 import { PipeTransform } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
 export declare class MockDatePipe extends DatePipe implements PipeTransform {
     transform(value: null | undefined, args?: string): null;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<MockDatePipe, never>;
+    static ɵpipe: ɵngcc0.ɵɵPipeDefWithMeta<MockDatePipe, "cxDate">;
 }
+
+//# sourceMappingURL=mock-date.pipe.d.ts.map
