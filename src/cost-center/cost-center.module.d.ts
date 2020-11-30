42395aba6fbda274285d094647a9fc814a41bd04
GitHub <noreply@github.com>
2020-11-30 11:15:36 +0000

    fe0fc5614 feat: My Company Status E2E Tests (#9852)

--- src/cost-center/cost-center.module.d.ts
+++ src/cost-center/cost-center.module.d.ts
@@ -1,9 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
 export declare class CostCenterModule {
     static forRoot(): ModuleWithProviders<CostCenterModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CostCenterModule, never, never, never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CostCenterModule>;
 }
-
-//# sourceMappingURL=cost-center.module.d.ts.map
