0adf0587a8fe2afd1f193d5b09cb0f7c77692cba
GitHub <noreply@github.com>
2020-11-30 11:50:27 +0000

    e2295066c test: E2E test for user role assignments from unit menu (#10006)

--- src/user/user.module.d.ts
+++ src/user/user.module.d.ts
@@ -1,4 +1,10 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './store/user-store.module';
 export declare class UserModule {
     static forRoot(): ModuleWithProviders<UserModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<UserModule, never, [typeof ɵngcc1.UserStoreModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<UserModule>;
 }
+
+//# sourceMappingURL=user.module.d.ts.map
