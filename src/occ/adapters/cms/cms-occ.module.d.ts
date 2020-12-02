7afa1e7a304d2e00abfe66fb6113446965f86110
GitHub <noreply@github.com>
2020-12-02 15:08:50 +0000

    d42753bd1 feat: Add RoutingParamsService to the public API. (GH-10137)(#10159)

--- src/occ/adapters/cms/cms-occ.module.d.ts
+++ src/occ/adapters/cms/cms-occ.module.d.ts
@@ -1,8 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
 export declare class CmsOccModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CmsOccModule, never, [typeof ɵngcc1.CommonModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CmsOccModule>;
 }
-
-//# sourceMappingURL=cms-occ.module.d.ts.map
