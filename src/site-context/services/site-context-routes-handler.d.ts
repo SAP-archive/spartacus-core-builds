add4ffa836fb9c8504ef2da4ce1eb49b4e2f6ba9
GitHub <noreply@github.com>
2020-12-03 12:41:15 +0000

    e4e5d3727 fix: Set proper page context for notFound page logic (#10178)

--- src/site-context/services/site-context-routes-handler.d.ts
+++ src/site-context/services/site-context-routes-handler.d.ts
@@ -1,7 +1,6 @@
 import { Injector, OnDestroy } from '@angular/core';
 import { SiteContextParamsService } from './site-context-params.service';
 import { SiteContextUrlSerializer } from './site-context-url-serializer';
-import * as ɵngcc0 from '@angular/core';
 export declare class SiteContextRoutesHandler implements OnDestroy {
     private siteContextParams;
     private serializer;
@@ -38,7 +37,4 @@ export declare class SiteContextRoutesHandler implements OnDestroy {
      */
     private setContextParamsFromRoute;
     ngOnDestroy(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<SiteContextRoutesHandler, never>;
 }
-
-//# sourceMappingURL=site-context-routes-handler.d.ts.map
