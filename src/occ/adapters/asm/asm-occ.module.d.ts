573cf1c47211752cd9bf9105824e40e3e67240d3
GitHub <noreply@github.com>
2020-12-03 12:05:38 +0000

    b92a7c043 fix: Message service notifications are layered on each other #9798 (#10051)

--- src/occ/adapters/asm/asm-occ.module.d.ts
+++ src/occ/adapters/asm/asm-occ.module.d.ts
@@ -1,8 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
 export declare class AsmOccModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AsmOccModule, never, [typeof ɵngcc1.CommonModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<AsmOccModule>;
 }
-
-//# sourceMappingURL=asm-occ.module.d.ts.map
