ca890f747804558ef79ca818f4d8b62ba5b8c947
GitHub <noreply@github.com>
2020-12-03 14:58:00 +0000

    ee7fc8d29 chore: Create METHOD migration for PageMetaService (GH-10139)

--- src/util/glob.service.d.ts
+++ src/util/glob.service.d.ts
@@ -1,3 +1,4 @@
+import * as ɵngcc0 from '@angular/core';
 export declare class GlobService {
     /**
      * For given list of glob-like patterns, returns a validator function.
@@ -5,4 +6,7 @@ export declare class GlobService {
      * The validator returns true for given URL only when ANY of the positive patterns is matched and NONE of the negative ones.
      */
     getValidator(patterns: string[]): (url: string) => boolean;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<GlobService, never>;
 }
+
+//# sourceMappingURL=glob.service.d.ts.map
