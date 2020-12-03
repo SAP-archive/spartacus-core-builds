06042d143a258f90c15cfadd753a4d89829b6e4a
GitHub <noreply@github.com>
2020-12-03 21:48:34 +0000

    7dcab8281 chore: add ssr migration for 3.0 (#10187)

--- src/features-config/directives/feature.directive.d.ts
+++ src/features-config/directives/feature.directive.d.ts
@@ -1,6 +1,5 @@
 import { TemplateRef, ViewContainerRef } from '@angular/core';
 import { FeatureConfigService } from '../services/feature-config.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class FeatureDirective {
     protected templateRef: TemplateRef<any>;
     protected viewContainer: ViewContainerRef;
@@ -8,8 +7,4 @@ export declare class FeatureDirective {
     constructor(templateRef: TemplateRef<any>, viewContainer: ViewContainerRef, featureConfig: FeatureConfigService);
     private hasView;
     set cxFeature(feature: string);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<FeatureDirective, never>;
-    static ɵdir: ɵngcc0.ɵɵDirectiveDefWithMeta<FeatureDirective, "[cxFeature]", never, { "cxFeature": "cxFeature"; }, {}, never>;
 }
-
-//# sourceMappingURL=feature.directive.d.ts.map
