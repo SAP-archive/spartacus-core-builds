ece6c922f5ce83b6279b0b0edb990f9c3986063c
GitHub <noreply@github.com>
2020-12-04 10:39:23 +0000

    021820078 feat: allow for providing subtitle in cx-org-card (#10217)

--- src/global-message/store/effects/global-message.effect.d.ts
+++ src/global-message/store/effects/global-message.effect.d.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { GlobalMessageConfig } from '../../config/global-message-config';
 import { GlobalMessageActions } from '../actions/index';
 import { StateWithGlobalMessage } from '../global-message-state';
+import * as ɵngcc0 from '@angular/core';
 export declare class GlobalMessageEffect {
     private actions$;
     private store;
@@ -12,4 +13,8 @@ export declare class GlobalMessageEffect {
     removeDuplicated$: Observable<GlobalMessageActions.RemoveMessage>;
     hideAfterDelay$: Observable<GlobalMessageActions.RemoveMessage>;
     constructor(actions$: Actions, store: Store<StateWithGlobalMessage>, config: GlobalMessageConfig, platformId: any);
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<GlobalMessageEffect, never>;
+    static ɵprov: ɵngcc0.ɵɵInjectableDef<GlobalMessageEffect>;
 }
+
+//# sourceMappingURL=global-message.effect.d.ts.map
