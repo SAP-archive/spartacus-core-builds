fc8c5fc28cab9928e0658db40e88075554698ca7
GitHub <noreply@github.com>
2020-12-04 10:53:51 +0000

    021820078 feat: allow for providing subtitle in cx-org-card (#10217)

--- src/user/connectors/customer-coupon/customer-coupon.connector.d.ts
+++ src/user/connectors/customer-coupon/customer-coupon.connector.d.ts
@@ -1,7 +1,6 @@
 import { Observable } from 'rxjs';
 import { CustomerCouponSearchResult, CustomerCouponNotification, CustomerCoupon2Customer } from '../../../model/customer-coupon.model';
 import { CustomerCouponAdapter } from './customer-coupon.adapter';
-import * as ɵngcc0 from '@angular/core';
 export declare class CustomerCouponConnector {
     protected adapter: CustomerCouponAdapter;
     constructor(adapter: CustomerCouponAdapter);
@@ -9,7 +8,4 @@ export declare class CustomerCouponConnector {
     turnOnNotification(userId: string, couponCode: string): Observable<CustomerCouponNotification>;
     turnOffNotification(userId: string, couponCode: string): Observable<{}>;
     claimCustomerCoupon(userId: string, couponCode: string): Observable<CustomerCoupon2Customer>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CustomerCouponConnector, never>;
 }
-
-//# sourceMappingURL=customer-coupon.connector.d.ts.map
