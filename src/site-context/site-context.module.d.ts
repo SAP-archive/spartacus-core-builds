d499d6bb2c170cb88541be60e2493f64c5fab65f
GitHub <noreply@github.com>
2020-12-04 14:47:53 +0000

    08bb4d69a fix: Add missing possibility for remove user group #10053 + delete address confirmation (#10075)

--- src/site-context/site-context.module.d.ts
+++ src/site-context/site-context.module.d.ts
@@ -1,11 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '../state/state.module';
-import * as ɵngcc2 from './store/site-context-store.module';
 export declare class SiteContextModule {
     static forRoot(): ModuleWithProviders<SiteContextModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<SiteContextModule, never, [typeof ɵngcc1.StateModule, typeof ɵngcc2.SiteContextStoreModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<SiteContextModule>;
 }
-
-//# sourceMappingURL=site-context.module.d.ts.map
