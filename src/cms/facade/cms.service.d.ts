f51bbf360f721a90019dfbf36b1f2371e64131fa
GitHub <noreply@github.com>
2020-12-04 18:41:13 +0000

    31ff35192 fix: replace map with forEach (#8823) (#9833)

--- src/cms/facade/cms.service.d.ts
+++ src/cms/facade/cms.service.d.ts
@@ -7,7 +7,6 @@ import { ContentSlotData } from '../model/content-slot-data.model';
 import { NodeItem } from '../model/node-item.model';
 import { Page } from '../model/page.model';
 import { StateWithCms } from '../store/cms-state';
-import * as ɵngcc0 from '@angular/core';
 export declare class CmsService {
     protected store: Store<StateWithCms>;
     protected routingService: RoutingService;
@@ -88,7 +87,4 @@ export declare class CmsService {
     getPage(pageContext: PageContext, forceReload?: boolean): Observable<Page>;
     getPageIndex(pageContext: PageContext): Observable<string>;
     setPageFailIndex(pageContext: PageContext, value: string): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CmsService, never>;
 }
-
-//# sourceMappingURL=cms.service.d.ts.map
--- src/site-context/store/effects/currencies.effect.d.ts
+++ src/site-context/store/effects/currencies.effect.d.ts
@@ -5,7 +5,6 @@ import { WindowRef } from '../../../window/window-ref';
 import { SiteConnector } from '../../connectors/site.connector';
 import { SiteContextActions } from '../actions/index';
 import { StateWithSiteContext } from '../state';
-import * as ɵngcc0 from '@angular/core';
 export declare class CurrenciesEffects {
     private actions$;
     private siteConnector;
@@ -15,8 +14,4 @@ export declare class CurrenciesEffects {
     persist$: Observable<void>;
     activateCurrency$: Observable<SiteContextActions.CurrencyChange>;
     constructor(actions$: Actions, siteConnector: SiteConnector, winRef: WindowRef, state: Store<StateWithSiteContext>);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CurrenciesEffects, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<CurrenciesEffects>;
 }
-
-//# sourceMappingURL=currencies.effect.d.ts.map
