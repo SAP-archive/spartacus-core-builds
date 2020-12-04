c6dff4f77c00c52041b7e33ae298d3fb0967070d
GitHub <noreply@github.com>
2020-12-04 09:27:48 +0000

    ef1cca500 fix: Drop usage of deprecated makeErrorSerializable (#10215)

--- src/user/store/effects/clear-miscs-data.effect.d.ts
+++ src/user/store/effects/clear-miscs-data.effect.d.ts
@@ -1,13 +1,8 @@
 import { Actions } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import * as ɵngcc0 from '@angular/core';
 export declare class ClearMiscsDataEffect {
     private actions$;
     clearMiscsData$: Observable<Action>;
     constructor(actions$: Actions);
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<ClearMiscsDataEffect, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<ClearMiscsDataEffect>;
 }
-
-//# sourceMappingURL=clear-miscs-data.effect.d.ts.map
