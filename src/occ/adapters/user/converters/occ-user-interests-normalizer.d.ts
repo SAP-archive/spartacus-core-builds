c7e6e04fe7f926509773535e4ad906a0f065fc65
GitHub <noreply@github.com>
2020-12-08 18:39:41 +0000

    c87d9a352 chore: temporarily skip failing b2b regression tests (#10272)

--- src/occ/adapters/user/converters/occ-user-interests-normalizer.d.ts
+++ src/occ/adapters/user/converters/occ-user-interests-normalizer.d.ts
@@ -1,8 +1,12 @@
 import { Occ } from '../../../occ-models/occ.models';
 import { Converter, ConverterService } from '../../../../util/converter.service';
 import { ProductInterestSearchResult } from '../../../../model/product-interest.model';
+import * as ɵngcc0 from '@angular/core';
 export declare class OccUserInterestsNormalizer implements Converter<Occ.ProductInterestSearchResult, ProductInterestSearchResult> {
     private converter;
     constructor(converter: ConverterService);
     convert(source: Occ.ProductInterestSearchResult, target?: ProductInterestSearchResult): ProductInterestSearchResult;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccUserInterestsNormalizer, never>;
 }
+
+//# sourceMappingURL=occ-user-interests-normalizer.d.ts.map
