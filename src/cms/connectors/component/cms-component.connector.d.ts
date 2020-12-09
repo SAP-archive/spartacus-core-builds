5ecb1b90aaa7ba56309d1b08627d466268f0c14d
GitHub <noreply@github.com>
2020-12-09 15:05:30 +0000

    2bb629eec test: fix replenishment tabbing in e2e (#10270)

--- src/cms/connectors/component/cms-component.connector.d.ts
+++ src/cms/connectors/component/cms-component.connector.d.ts
@@ -4,7 +4,6 @@ import { OccConfig } from '../../../occ/config/occ-config';
 import { PageContext } from '../../../routing/models/page-context.model';
 import { CmsStructureConfigService } from '../../services/cms-structure-config.service';
 import { CmsComponentAdapter } from './cms-component.adapter';
-import * as ɵngcc0 from '@angular/core';
 export declare class CmsComponentConnector {
     protected cmsStructureConfigService: CmsStructureConfigService;
     protected cmsComponentAdapter: CmsComponentAdapter;
@@ -12,7 +11,4 @@ export declare class CmsComponentConnector {
     constructor(cmsStructureConfigService: CmsStructureConfigService, cmsComponentAdapter: CmsComponentAdapter, config: OccConfig);
     get<T extends CmsComponent>(id: string, pageContext: PageContext): Observable<T>;
     getList(ids: string[], pageContext: PageContext): Observable<CmsComponent[]>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<CmsComponentConnector, never>;
 }
-
-//# sourceMappingURL=cms-component.connector.d.ts.map
