00a937371120df76d7ee4086bdf637da702c0245
GitHub <noreply@github.com>
2020-12-16 15:05:39 +0000

    23b5ffdcd fix: replace multi-line comments by single line comments (#10450)

--- src/occ/adapters/cost-center/converters/occ-cost-center-list-normalizer.d.ts
+++ src/occ/adapters/cost-center/converters/occ-cost-center-list-normalizer.d.ts
@@ -2,8 +2,12 @@ import { EntitiesModel } from '../../../../model/misc.model';
 import { CostCenter } from '../../../../model/org-unit.model';
 import { Converter, ConverterService } from '../../../../util/converter.service';
 import { Occ } from '../../../occ-models/occ.models';
+import * as ɵngcc0 from '@angular/core';
 export declare class OccCostCenterListNormalizer implements Converter<Occ.CostCentersList, EntitiesModel<CostCenter>> {
     private converter;
     constructor(converter: ConverterService);
     convert(source: Occ.CostCentersList, target?: EntitiesModel<CostCenter>): EntitiesModel<CostCenter>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCostCenterListNormalizer, never>;
 }
+
+//# sourceMappingURL=occ-cost-center-list-normalizer.d.ts.map
