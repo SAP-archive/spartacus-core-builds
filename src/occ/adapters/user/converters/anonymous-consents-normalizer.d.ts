01c0a3808f196ff5fe83f368be4b2c099ddc060c
GitHub <noreply@github.com>
2020-12-16 15:15:17 +0000

    23b5ffdcd fix: replace multi-line comments by single line comments (#10450)

--- src/occ/adapters/user/converters/anonymous-consents-normalizer.d.ts
+++ src/occ/adapters/user/converters/anonymous-consents-normalizer.d.ts
@@ -1,12 +1,8 @@
 import { AnonymousConsentsService } from '../../../../anonymous-consents/facade/anonymous-consents.service';
 import { AnonymousConsent } from '../../../../model/consent.model';
 import { Converter } from '../../../../util/converter.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class AnonymousConsentNormalizer implements Converter<string, AnonymousConsent[]> {
     protected anonymousConsentsService: AnonymousConsentsService;
     constructor(anonymousConsentsService: AnonymousConsentsService);
     convert(source: string): AnonymousConsent[];
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AnonymousConsentNormalizer, never>;
 }
-
-//# sourceMappingURL=anonymous-consents-normalizer.d.ts.map
