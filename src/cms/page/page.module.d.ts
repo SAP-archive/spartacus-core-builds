571abe454ad6714ca1c904f3c3166e1c6473bd6f
GitHub <noreply@github.com>
2020-12-17 22:30:34 +0000

    8152c3077 feat: Assign/Unassign Approvers works in random order #10468 (#10491)

--- src/cms/page/page.module.d.ts
+++ src/cms/page/page.module.d.ts
@@ -1,7 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
 export declare class CmsPageTitleModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CmsPageTitleModule, never, never, never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CmsPageTitleModule>;
 }
-
-//# sourceMappingURL=page.module.d.ts.map
