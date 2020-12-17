ba512fc6116da5c2016d7b8e903fe9e477e1a1c9
GitHub <noreply@github.com>
2020-12-17 13:24:02 +0000

    c4b8a0ffc feat: Expose core style capabilities (#10463)

--- src/auth/user-auth/services/auth-redirect-storage.service.d.ts
+++ src/auth/user-auth/services/auth-redirect-storage.service.d.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
  * Service serves storage role for AuthRedirectService.
  * Used by AuthStatePersistenceService to store redirect url for OAuth flows that rely on redirects.
  */
-import * as ɵngcc0 from '@angular/core';
 export declare class AuthRedirectStorageService {
     constructor();
     private redirectUrl$;
@@ -19,7 +18,4 @@ export declare class AuthRedirectStorageService {
      * @param redirectUrl
      */
     setRedirectUrl(redirectUrl: string): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<AuthRedirectStorageService, never>;
 }
-
-//# sourceMappingURL=auth-redirect-storage.service.d.ts.map
