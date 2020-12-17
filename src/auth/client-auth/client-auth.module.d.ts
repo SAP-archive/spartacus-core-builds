592be3f4014a540de086e02806ef5c3033062fa4
GitHub <noreply@github.com>
2020-12-17 19:22:42 +0000

    d5d982b95 fix: Date range validation doesn't work properly #10401 (#10453)

--- src/auth/client-auth/client-auth.module.d.ts
+++ src/auth/client-auth/client-auth.module.d.ts
@@ -6,6 +6,13 @@ import { ModuleWithProviders } from '@angular/core';
  *
  * After OCC improvements regarding client authentication this module can be safely removed.
  */
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from '@angular/common';
+import * as ɵngcc2 from './store/client-auth-store.module';
 export declare class ClientAuthModule {
     static forRoot(): ModuleWithProviders<ClientAuthModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ClientAuthModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.ClientAuthStoreModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ClientAuthModule>;
 }
+
+//# sourceMappingURL=client-auth.module.d.ts.map
--- src/checkout/connectors/payment/checkout-payment.connector.d.ts
+++ src/checkout/connectors/payment/checkout-payment.connector.d.ts
@@ -1,10 +1,14 @@
 import { Observable } from 'rxjs';
 import { CheckoutPaymentAdapter } from './checkout-payment.adapter';
 import { CardType, PaymentDetails } from '../../../model/cart.model';
+import * as ɵngcc0 from '@angular/core';
 export declare class CheckoutPaymentConnector {
     protected adapter: CheckoutPaymentAdapter;
     constructor(adapter: CheckoutPaymentAdapter);
     create(userId: string, cartId: string, paymentDetails: PaymentDetails): Observable<PaymentDetails>;
     set(userId: string, cartId: string, paymentDetailsId: string): Observable<any>;
     getCardTypes(): Observable<CardType[]>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<CheckoutPaymentConnector, never>;
 }
+
+//# sourceMappingURL=checkout-payment.connector.d.ts.map
