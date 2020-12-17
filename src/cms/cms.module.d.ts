1c49e610915de2e56d4db03239dcb980378f6282
GitHub <noreply@github.com>
2020-12-17 14:22:13 +0000

    5705e8db2 fix: Handle missing `occ.baseUrl` in `AuthConfigService` (#10437)

--- src/cms/cms.module.d.ts
+++ src/cms/cms.module.d.ts
@@ -1,11 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './store/cms-store.module';
-import * as ɵngcc2 from './page/page.module';
 export declare class CmsModule {
     static forRoot(): ModuleWithProviders<CmsModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<CmsModule, never, [typeof ɵngcc1.CmsStoreModule, typeof ɵngcc2.CmsPageTitleModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<CmsModule>;
 }
-
-//# sourceMappingURL=cms.module.d.ts.map
