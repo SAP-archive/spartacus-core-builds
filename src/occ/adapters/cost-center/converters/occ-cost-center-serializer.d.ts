7013f57affd4650ce3bafc6996bf812c0705840f
GitHub <noreply@github.com>
2020-12-17 20:37:20 +0000

    64d8143c7 feat: Unit shipping address - form data is not clearing after submit #10464 (#10488)

--- src/occ/adapters/cost-center/converters/occ-cost-center-serializer.d.ts
+++ src/occ/adapters/cost-center/converters/occ-cost-center-serializer.d.ts
@@ -1,6 +1,10 @@
 import { CostCenter } from '../../../../model/org-unit.model';
 import { Converter } from '../../../../util/converter.service';
 import { Occ } from '../../../occ-models/occ.models';
+import * as ɵngcc0 from '@angular/core';
 export declare class OccCostCenterSerializer implements Converter<CostCenter, Occ.CostCenter> {
     convert(source: CostCenter, target?: Occ.CostCenter): Occ.CostCenter;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCostCenterSerializer, never>;
 }
+
+//# sourceMappingURL=occ-cost-center-serializer.d.ts.map
