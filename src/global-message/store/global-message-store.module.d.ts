d2b20ca6e36fcf941e2037f356664d1da31a4e96
GitHub <noreply@github.com>
2020-12-21 14:42:31 +0000

    a09a2914f fix: Cannot edit budget (missing context for pattern & placeholder ) (#10511)

--- src/global-message/store/global-message-store.module.d.ts
+++ src/global-message/store/global-message-store.module.d.ts
@@ -1,9 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '../../state/state.module';
-import * as ɵngcc2 from '@ngrx/store';
 export declare class GlobalMessageStoreModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<GlobalMessageStoreModule, never, [typeof ɵngcc1.StateModule, typeof ɵngcc2.StoreFeatureModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<GlobalMessageStoreModule>;
 }
-
-//# sourceMappingURL=global-message-store.module.d.ts.map
--- src/routing/configurable-routes/url-translation/url.module.d.ts
+++ src/routing/configurable-routes/url-translation/url.module.d.ts
@@ -1,10 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './url.pipe';
-import * as ɵngcc2 from './product-url.pipe';
-import * as ɵngcc3 from '@angular/common';
 export declare class UrlModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<UrlModule, [typeof ɵngcc1.UrlPipe, typeof ɵngcc2.ProductURLPipe], [typeof ɵngcc3.CommonModule], [typeof ɵngcc1.UrlPipe, typeof ɵngcc2.ProductURLPipe]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<UrlModule>;
 }
-
-//# sourceMappingURL=url.module.d.ts.map
