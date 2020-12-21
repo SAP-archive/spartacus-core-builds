64588bdbb6980b5890e63e796478d2d5c3b41f80
GitHub <noreply@github.com>
2020-12-21 12:00:15 +0000

    0b7cded0e fix: show global message when no customerId provided on session start (#10506)

--- src/product/product.module.d.ts
+++ src/product/product.module.d.ts
@@ -1,4 +1,10 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './store/product-store.module';
 export declare class ProductModule {
     static forRoot(): ModuleWithProviders<ProductModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<ProductModule, never, [typeof ɵngcc1.ProductStoreModule], never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<ProductModule>;
 }
+
+//# sourceMappingURL=product.module.d.ts.map
