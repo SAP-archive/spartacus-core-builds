dc18d24a067fbb4208f188fcce1e90b010a8a07f
GitHub <noreply@github.com>
2021-01-08 15:16:56 +0000

    5e0e65955 feat: login and logout event (#10627)

--- src/auth/user-auth/events/login-event.builder.d.ts
+++ src/auth/user-auth/events/login-event.builder.d.ts
@@ -1,5 +1,4 @@
 import { StateEventService } from '../../../state/event/state-event.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class LoginEventBuilder {
     protected stateEventService: StateEventService;
     constructor(stateEventService: StateEventService);
@@ -11,7 +10,4 @@ export declare class LoginEventBuilder {
      * Register a logout event
      */
     protected loginEvent(): void;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<LoginEventBuilder, never>;
 }
-
-//# sourceMappingURL=login-event.builder.d.ts.map
