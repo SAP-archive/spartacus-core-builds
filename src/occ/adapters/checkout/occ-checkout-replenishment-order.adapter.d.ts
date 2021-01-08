46f62556fb23e3702dbc39962c8768ab7a237412
GitHub <noreply@github.com>
2021-01-08 13:09:57 +0000

    1c16e19eb feat: auto-generate code when creating entity in organization (#10616)

--- src/occ/adapters/checkout/occ-checkout-replenishment-order.adapter.d.ts
+++ src/occ/adapters/checkout/occ-checkout-replenishment-order.adapter.d.ts
@@ -4,15 +4,10 @@ import { CheckoutReplenishmentOrderAdapter } from '../../../checkout/index';
 import { ReplenishmentOrder, ScheduleReplenishmentForm } from '../../../model/replenishment-order.model';
 import { ConverterService } from '../../../util/converter.service';
 import { OccEndpointsService } from '../../services/occ-endpoints.service';
-import * as ɵngcc0 from '@angular/core';
 export declare class OccCheckoutReplenishmentOrderAdapter implements CheckoutReplenishmentOrderAdapter {
     protected http: HttpClient;
     protected occEndpoints: OccEndpointsService;
     protected converter: ConverterService;
     constructor(http: HttpClient, occEndpoints: OccEndpointsService, converter: ConverterService);
     scheduleReplenishmentOrder(cartId: string, scheduleReplenishmentForm: ScheduleReplenishmentForm, termsChecked: boolean, userId: string): Observable<ReplenishmentOrder>;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<OccCheckoutReplenishmentOrderAdapter, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDef<OccCheckoutReplenishmentOrderAdapter>;
 }
-
-//# sourceMappingURL=occ-checkout-replenishment-order.adapter.d.ts.map
